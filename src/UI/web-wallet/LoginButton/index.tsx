c99b4abb32a6b7a8ec82357d2cb790047e71f4ad
eus <iagarueustatiu@gmail.com>
2021-12-07 10:47:59 +0200

    [+] adds extension & web wallet login services [+] login buttons

---
+++ src/UI/web-wallet/LoginButton/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import classNames from 'classnames';
+import webWalletLogin from '../../../services/login/webWalletLogin';
+import { Props } from './types';
+
+export const WebWalletLoginButton: (props: Props) => JSX.Element = ({
+    token,
+    className,
+    callbackRoute,
+    loginButtonText,
+    shouldRenderDefaultCss = true,
+}) => {
+
+    const generatedClasses = {
+
+        wrapper: classNames(className, {
+            "btn btn-primary px-sm-4 m-1 mx-sm-3": shouldRenderDefaultCss
+        }),
+
+        loginText: classNames(`${className}_login-text`, {
+            "text-left": shouldRenderDefaultCss
+        }),
+
+    };
+
+    const handleLogin = () => {
+
+        webWalletLogin({
+            token,
+            callbackRoute
+        });
+
+    };
+
+    return (
+        
+        <button onClick={handleLogin} className={generatedClasses.wrapper}>
+            <span className={generatedClasses.loginText}>
+                {loginButtonText}
+            </span>
+        </button>
+
+    )
+
+}
+
+
+export default WebWalletLoginButton;
