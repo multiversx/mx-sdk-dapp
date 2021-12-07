80a7ac266fe3097acedc838a2d622ac896cf8da9
eus <iagarueustatiu@gmail.com>
2021-12-07 15:58:19 +0200

    [*] fix src/index export

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import { DappProvider } from './redux/DappProvider';
 import { AuthenticatedRoutesWrapper } from './wrappers';
+
 // import styles from './styles.module.css';
 
 export * as loginServices from './services/login';
-
-
 export { DappProvider, AuthenticatedRoutesWrapper };
