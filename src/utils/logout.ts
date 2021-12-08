ad25dc4b2a16d51ddb66230311fe8a8fc346485f
Stanislav Sava <sava.stanislav@gmail.com>
2021-12-08 15:13:24 +0200

    fix logout button

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -5,6 +5,9 @@ import { store } from '../redux/store';
 export function logout(callbackUrl?: string) {
   const provider = providerSelector(store.getState());
   provider.logout({ callbackUrl }).then(() => {
+    if (callbackUrl != null && window.location.href != callbackUrl) {
+      window.location.href = callbackUrl;
+    }
     store.dispatch(logoutAction());
   });
 }
