ff0cc474c6a21cb45d0c7187af149eb7217679ae
Tudor Morar <tudor.morar@elrond.com>
2022-01-03 16:16:40 +0300

    Stop using getIsLoggedIn

--- example/src/components/Layout/Navbar/index.tsx
+++ example/src/components/Layout/Navbar/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Navbar as BsNavbar, NavItem, Nav } from 'react-bootstrap';
-import { logout, getIsLoggedIn } from 'dapp-core';
+import { logout, useGetAccountInfo } from 'dapp-core';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ElrondLogo } from './../../../assets/img/elrond.svg';
 import { dAppName } from 'config';
 import { routeNames } from 'routes';
 
 const Navbar = () => {
+  const { address } = useGetAccountInfo();
+
   const handleLogout = () => {
     logout(`${window.location.origin}/unlock`);
   };
 
-  const isLoggedIn = getIsLoggedIn();
+  const isLoggedIn = Boolean(address);
 
   return (
     <BsNavbar className='bg-white border-bottom px-4 py-3'>
