64f4d944e9ebe3532bd31903e7cd6b0f5f22d492
Stanislav Sava <sava.stanislav@gmail.com>
2022-01-11 16:51:19 +0200

    add networkConfig getter

--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -2,3 +2,5 @@ export * from './getNetworkProxy';
 export * from './getApiProvider';
 export * from './getAccountProviderType';
 export * from './getAccountProvider';
+export * from './getChainID';
+export * from './getNetworkConfig';
