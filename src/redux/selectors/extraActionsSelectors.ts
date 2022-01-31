9f0bbe19b3d9bef869129459bf7dab32dbfeed58
Tudor Morar <tudor.morar@elrond.com>
2022-01-31 18:38:34 +0300

    Remove unused selector

--- src/redux/selectors/extraActionsSelectors.ts
+++ src/redux/selectors/extraActionsSelectors.ts
@@ -3,11 +3,6 @@ import { RootState } from '../store';
 
 export const extraActionsSelector = (state: RootState) => state.extraActions;
 
-export const extraSignTransactionsSelector = createSelector(
-  extraActionsSelector,
-  (state) => state.extraSignTransactions
-);
-
 export const extraActionsInit = createSelector(
   extraActionsSelector,
   (state) => state.init
