97a05a4be3bbd675b8fbe0969013a6deaf5ef479
Tudor Morar <tudor.morar@elrond.com>
2022-04-04 17:14:12 +0300

    [-] Change to correct expires time

--- src/storage/local.ts
+++ src/storage/local.ts
@@ -2,7 +2,7 @@ import storage from 'utils/storage';
 import { localStorageKeys } from 'utils/storage/local';
 
 export function getNewLoginExpiresTimestamp() {
-  return new Date().setSeconds(new Date().getSeconds() + 24);
+  return new Date().setHours(new Date().getHours() + 24);
 }
 
 export function setLoginExpiresAt(expiresAt: number) {
