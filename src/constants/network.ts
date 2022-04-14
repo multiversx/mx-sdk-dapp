a48231609e1722ac5804fdaf43ec11304d63eb6e
Tudor Morar <tudor.morar@elrond.com>
2022-04-14 12:48:30 +0300

    Fix fallbackNetworkConfigurations type

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,11 +1,11 @@
-import { NetworkType } from 'types';
+import { EnvironmentsEnum, NetworkType } from 'types';
 
 export const DEFAULT_MIN_GAS_LIMIT = 50_000;
 
 export const configEndpoint = 'dapp/config';
 
 export const fallbackNetworkConfigurations: Record<
-  'mainnet' | 'testnet' | 'devnet',
+  keyof typeof EnvironmentsEnum,
   NetworkType
 > = {
   devnet: {
