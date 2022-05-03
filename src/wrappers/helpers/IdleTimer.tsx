7679ddb71e8d29ad325f1bf00b92131d323aa1bc
Tudor Morar <tudor.morar@elrond.com>
2022-05-03 17:49:03 +0300

    [*] Increase idleTimer timeout to 10 minutes

--- src/wrappers/helpers/IdleTimer.tsx
+++ src/wrappers/helpers/IdleTimer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useIdleTimer } from 'react-idle-timer';
 import { getIsLoggedIn, logout } from 'utils';
 
-const minutes = 0.2;
+const minutes = 10;
 const timeout = 1000 * 60 * minutes;
 const debounce = 500;
 
@@ -15,11 +15,7 @@ const IdleTimer = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
-  const { isIdle } = useIdleTimer({ timeout, onIdle, debounce });
-
-  if (isIdle() && isLoggedIn) {
-    return null;
-  }
+  useIdleTimer({ timeout, onIdle, debounce });
 
   return <>{children}</>;
 };
