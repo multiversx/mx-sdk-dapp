8c22626ddbc978be3bae98a95ffb726ae80a8594
Tudor Morar <tudor.morar@elrond.com>
2022-05-03 14:46:29 +0300

    Resolve comments

--- src/wrappers/helpers/IdleTimer.tsx
+++ src/wrappers/helpers/IdleTimer.tsx
@@ -2,26 +2,25 @@ import React from 'react';
 import { useIdleTimer } from 'react-idle-timer';
 import { getIsLoggedIn, logout } from 'utils';
 
-const minutes = 10;
+const minutes = 0.2;
+const timeout = 1000 * 60 * minutes;
+const debounce = 500;
 
 const IdleTimer = ({ children }: { children: React.ReactNode }) => {
   const isLoggedIn = getIsLoggedIn();
 
-  const handleOnIdle = () => {
+  const onIdle = () => {
     if (isLoggedIn) {
       logout();
     }
   };
 
-  const { isIdle } = useIdleTimer({
-    timeout: 1000 * 60 * minutes,
-    onIdle: handleOnIdle,
-    debounce: 500
-  });
+  const { isIdle } = useIdleTimer({ timeout, onIdle, debounce });
 
   const userIsActive = !isIdle();
+  const userIsOnPublicRoute = !isLoggedIn;
 
-  if (userIsActive || !isLoggedIn) {
+  if (userIsActive || userIsOnPublicRoute) {
     return <>{children}</>;
   }
 
