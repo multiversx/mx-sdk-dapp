e4cf83401b89e8e814161115d2b5774f0a9737f0
Tudor Morar <tudor.morar@elrond.com>
2022-05-04 15:58:15 +0300

    [+] Create SignWithDevice wrapper for ledger and extra provider

--- src/UI/SignTransactionsModals/SignWithLedgerModal/index.tsx
+++ src/UI/SignTransactionsModals/SignWithLedgerModal/index.tsx
@@ -1,69 +1,14 @@
 import React from 'react';
-import { useSignTransactionsWithLedger } from 'hooks';
-import classNames from 'optionalPackages/classnames';
-import ReactBootstrap from 'optionalPackages/react-bootstrap';
 import { SignModalPropsType } from 'types';
-import { getGeneratedClasses, wrapperClassName } from 'utils';
-import SignStep from './SignStep';
+import SignWithDeviceModal from '../SignWithDeviceModal';
 
-const SignWithLedgerModal = ({
-  handleClose,
-  error,
-  className = 'ledger-modal',
-  verifyReceiverScam = true,
-  title = 'Confirm on Ledger'
-}: SignModalPropsType) => {
-  const {
-    onSignTransaction,
-    onNext,
-    onPrev,
-    allTransactions,
-    waitingForDevice,
-    onAbort,
-    isLastTransaction,
-    currentStep,
-    callbackRoute,
-    currentTransaction
-  } = useSignTransactionsWithLedger({
-    onCancel: handleClose,
-    verifyReceiverScam
-  });
-  const classes = getGeneratedClasses(className, true, {
-    wrapper: 'modal-container wallet-connect',
-    container: 'card container',
-    cardBody: 'card-body'
-  });
+const SignWithLedgerModal = (props: SignModalPropsType) => {
   return (
-    <ReactBootstrap.Modal
-      show={currentTransaction != null}
-      backdrop='static'
-      onHide={handleClose}
-      className={classNames(classes.wrapper, wrapperClassName)}
-      animation={false}
-      centered
-    >
-      <div className={classes.container}>
-        <div className={classes.cardBody}>
-          <SignStep
-            {...{
-              onSignTransaction,
-              onNext,
-              onPrev,
-              allTransactions,
-              waitingForDevice,
-              isLastTransaction,
-              currentStep,
-              callbackRoute,
-              currentTransaction,
-              handleClose: onAbort,
-              className,
-              error,
-              title
-            }}
-          />
-        </div>
-      </div>
-    </ReactBootstrap.Modal>
+    <SignWithDeviceModal
+      {...props}
+      title={props.title || 'Confirm on Ledger'}
+      className={props.className || 'ledger-modal'}
+    />
   );
 };
 
