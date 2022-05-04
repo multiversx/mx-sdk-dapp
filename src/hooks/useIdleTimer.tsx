3c72873bc1c7db6ebb03d4def8cc92364a140da5
Tudor Morar <tudor.morar@elrond.com>
2022-05-04 12:18:03 +0300

    [+] Create useIdleTimer as hook

---
+++ src/hooks/useIdleTimer.tsx
@@ -0,0 +1,27 @@
+import { useIdleTimer as useReactIdleTimer } from 'react-idle-timer';
+import { getIsLoggedIn, logout as dappLogout } from 'utils';
+
+const getTimeout = (minutes: number) => 1000 * 60 * minutes;
+const debounce = 500;
+
+interface IdleTimerType {
+  minutes?: number;
+  onLogout?: (props?: any) => void;
+}
+
+export const useIdleTimer = ({ minutes = 10, onLogout }: IdleTimerType) => {
+  const isLoggedIn = getIsLoggedIn();
+  const defaultLogout = () => dappLogout();
+  const logout = onLogout ? onLogout : defaultLogout;
+  const timeout = getTimeout(minutes);
+
+  const onIdle = () => {
+    if (isLoggedIn) {
+      logout();
+    }
+  };
+
+  useReactIdleTimer({ timeout, onIdle, debounce });
+};
+
+export default useIdleTimer;
