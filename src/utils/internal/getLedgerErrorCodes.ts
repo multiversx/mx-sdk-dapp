b1df879f766cb5a0987aee50b4ef3b7dd5deafc0
Tudor Morar <tudor.morar@elrond.com>
2022-05-24 12:53:03 +0300

    [+] Include wrongCLA code in ledger app error message

--- src/utils/internal/getLedgerErrorCodes.ts
+++ src/utils/internal/getLedgerErrorCodes.ts
@@ -2,15 +2,17 @@ import ledgerErrorCodes from 'constants/ledgerErrorCodes';
 
 const ledgerAppErrorText = 'Check if Elrond app is open on Ledger';
 const notConnectedCode = 0x6e01;
+const wrongClaCode = 0x6e00;
+const inactiveAppCodes = [notConnectedCode, wrongClaCode];
 
 export function getLedgerErrorCodes(err?: any) {
   let errorMessage: string | null = null;
-  debugger;
   if (err?.statusCode in ledgerErrorCodes) {
     const statusCode: keyof typeof ledgerErrorCodes = err?.statusCode;
     const { message } = ledgerErrorCodes[statusCode];
-    errorMessage =
-      statusCode === notConnectedCode ? ledgerAppErrorText : message;
+    errorMessage = inactiveAppCodes.includes(statusCode)
+      ? ledgerAppErrorText
+      : message;
   }
   return {
     errorMessage,
