bed88e6a695f0d22ff92e255a6257ad986889898
Tudor Morar <tudor.morar@elrond.com>
2022-05-25 18:36:11 +0300

    [*] Lint timeRemaining

--- src/utils/operations/timeRemaining.ts
+++ src/utils/operations/timeRemaining.ts
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-export function dhms(ms: number) {
+export function getRemainingTime(ms: number) {
   const days = Math.floor(ms / (24 * 60 * 60 * 1000));
   const daysms = ms % (24 * 60 * 60 * 1000);
   const hrs = Math.floor(daysms / (60 * 60 * 1000));
@@ -9,36 +9,44 @@ export function dhms(ms: number) {
   const minsms = hrsms % (60 * 1000);
   const secs = Math.floor(minsms / 1000);
 
-  // let diff = ' ago';
-  const diff = '';
   let secsString = secs + ' sec';
   let minsString = mins + ' min';
   let hrsString = hrs + ' hr';
   let daysString = days + ' day';
 
-  if (secs > 1) secsString = secs + ' secs';
-  if (mins > 1) minsString = mins + ' mins';
-  if (hrs > 1) hrsString = hrs + ' hrs';
-  if (days > 1) daysString = days + ' days';
+  if (secs > 1) {
+    secsString = secs + ' secs';
+  }
+  if (mins > 1) {
+    minsString = mins + ' mins';
+  }
+  if (hrs > 1) {
+    hrsString = hrs + ' hrs';
+  }
+  if (days > 1) {
+    daysString = days + ' days';
+  }
 
-  if (days >= 1) return daysString + ' ' + hrsString + diff;
+  if (days >= 1) {
+    return daysString + ' ' + hrsString;
+  }
   if (hrs >= 1) {
-    const minutesString = mins === 0 ? '' : ' ' + minsString + diff;
+    const minutesString = mins === 0 ? '' : ' ' + minsString;
     return hrsString + minutesString;
   }
   if (mins >= 1) {
-    const secString = secs === 0 ? '' : ' ' + secsString + diff;
+    const secString = secs === 0 ? '' : ' ' + secsString;
     return minsString + secString;
   }
 
-  return secsString + diff;
+  return secsString;
 }
 
 export function timeRemaining(duration: number) {
   const startDate = moment.utc();
   const endDate = moment.utc().add(duration, 'seconds');
   const diffInMs = Math.max(endDate.diff(startDate), 0);
-  let remaning = dhms(diffInMs);
+  let remaning = getRemainingTime(diffInMs);
 
   const parts = remaning.split(' ');
   if (parts.length > 1) {
