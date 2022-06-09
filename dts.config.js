b8da35275f188aea9f3afb26d034589a4432027b
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-06-09 13:55:38 +0300

    use rollup postcss for scss modules bundling

--- dts.config.js
+++ dts.config.js
@@ -1,16 +1,22 @@
-const scss = require('rollup-plugin-scss');
 const svg = require('rollup-plugin-svg-import');
 const svgr = require('@svgr/rollup');
+const postcssmodules = require('rollup-plugin-postcss-modules').default;
+const autoprefixer = require('autoprefixer');
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+
 module.exports = {
   /**
    * @param {import('rollup/dist/rollup').InputOptions} config
    */
   rollup(config, options) {
+    config.plugins.push(peerDepsExternal());
     config.plugins.push(
-      scss({
-        output: './dist/index.css',
-        failOnError: true,
-        runtime: require('sass')
+      postcssmodules({
+        extract: false,
+        plugins: [autoprefixer()],
+        writeDefinitions: true,
+        modules: true,
+        use: ['sass']
       })
     );
     config.plugins.push(svg());
