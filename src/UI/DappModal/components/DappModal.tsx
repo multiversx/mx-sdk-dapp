ddbafcf191e82d8311f9e3d709fb9f2588e89a21
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-06-10 17:36:37 +0300

    decorate DappModal with some className properties and add a bit of styling on modal

--- src/UI/DappModal/components/DappModal.tsx
+++ src/UI/DappModal/components/DappModal.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from '../styles/dapp-modal.scss';
+import icons from '../../../optionalPackages/fortawesome-free-solid-svg-icons';
+import ReactFontawesome from '../../../optionalPackages/react-fontawesome';
 
 type AwesomeModalProps = {
   visible: boolean;
@@ -8,6 +10,12 @@ type AwesomeModalProps = {
   showFooter?: boolean;
   headerText?: string;
   footerText?: string;
+  modalDialogClassName?: string;
+  modalContentClassName?: string;
+  modalHeaderClassName?: string;
+  modalCloseButtonClassName?: string;
+  modalBodyClassName?: string;
+  modalFooterClassName?: string;
   onHide: () => void;
   parentElement?: Element;
   children?: React.ReactNode;
@@ -20,30 +28,52 @@ const DappModal: React.FC<AwesomeModalProps> = ({
   showFooter,
   headerText,
   footerText,
+  modalDialogClassName,
+  modalContentClassName,
+  modalHeaderClassName,
+  modalCloseButtonClassName,
+  modalBodyClassName,
+  modalFooterClassName,
   parentElement,
   children
 }) => {
   return visible
     ? ReactDOM.createPortal(
-        <div className={styles.dappModal}>
+        <div
+          id='dapp-modal'
+          role='dialog'
+          aria-modal='true'
+          className={`${styles.dappModal} ${modalDialogClassName}`}
+          onClick={onHide}
+        >
           <div
-            className={styles.dappModalContent}
-            role='dialog'
-            aria-modal='true'
+            className={`${styles.dappModalContent} ${modalContentClassName}`}
           >
             {showHeader && (
-              <div className={styles.dappModalHeader}>
-                <span className={styles.dappModalClose} onClick={onHide}>
-                  &times;
-                </span>
-                <h2>{headerText}</h2>
+              <div
+                className={`${styles.dappModalHeader} ${modalHeaderClassName}`}
+              >
+                <div style={{ padding: '0 1rem' }}>{headerText}</div>
+                <button
+                  className={`${styles.dappModalCloseButton} ${modalCloseButtonClassName}`}
+                  onClick={onHide}
+                >
+                  <ReactFontawesome.FontAwesomeIcon
+                    size='lg'
+                    icon={icons.faTimes}
+                  />
+                </button>
               </div>
             )}
 
-            <div className={styles.dappModalBody}>{children}</div>
+            <div className={`${styles.dappModalBody} ${modalBodyClassName}`}>
+              {children}
+            </div>
 
             {showFooter && (
-              <div className={styles.dappModalFooter}>
+              <div
+                className={`${styles.dappModalFooter} ${modalFooterClassName}`}
+              >
                 <h3>{footerText}</h3>
               </div>
             )}
