d217c6febf99ca0fd49703c7c3342bd5584b269b
Grigore Budac <contact@grigorebudac.com>
2022-06-15 16:46:57 +0200

    use webpack for bundling the project

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require('path');
+
+const entryPoints = fs
+  .readdirSync('src')
+  .filter((files) => !files.includes('.'))
+  .map((folder) => {
+    const files = fs.readdirSync(`src/${folder}`);
+    const index = files.findIndex(
+      (file) => file.includes('index.ts') || file.includes('index.tsx')
+    );
+
+    if (index < 0) {
+      return undefined;
+    }
+
+    return `${folder}/${files[index]}`;
+  })
+  .filter((folder) => folder != null)
+  .reduce((acc, cv) => {
+    const withoutExtension = cv.substring(0, cv.indexOf('.'));
+    acc[withoutExtension] = `./src/${cv}`;
+    return acc;
+  }, {});
+
+console.log(entryPoints);
+
+module.exports = {
+  entry: entryPoints,
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      // react: 'React',
+      // 'react-native': 'ReactNative',
+      process: 'process/browser'
+    },
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      stream: false,
+      crypto: false
+    }
+  }
+};
