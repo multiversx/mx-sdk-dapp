2f611dede497e90b27fa3e8910cb3475ad5a7bab
Stanislav Sava <sava.stanislav@gmail.com>
2022-06-21 13:17:09 +0300

    replace sync reading with glob library when building

--- esbuild.js
+++ esbuild.js
@@ -1,47 +1,35 @@
 const svgrPlugin = require('esbuild-plugin-svgr');
 const esbuild = require('esbuild');
+const glob = require('glob');
 const plugin = require('node-stdlib-browser/helpers/esbuild/plugin');
 const stdLibBrowser = require('node-stdlib-browser');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
-const fs = require('fs');
 
-const entryPoints = fs
-  .readdirSync('src')
-  .filter((files) => !files.includes('.'))
-  .map((folder) => {
-    const files = fs.readdirSync(`src/${folder}`);
+glob('{./src/**/*.tsx,./src/**/*.ts}', function(err, files) {
+  if (err) {
+    console.log('error reading files', err);
+  }
 
-    const index = files.findIndex(
-      (file) => file.includes('index.ts') || file.includes('index.tsx')
-    );
-
-    if (index < 0) {
-      return undefined;
-    }
-
-    return `src/${folder}/${files[index]}`;
-  })
-  .filter((folder) => folder != null);
-
-esbuild
-  .build({
-    entryPoints,
-    outdir: 'dist',
-    bundle: true,
-    treeShaking: true,
-    minify: true,
-    sourcemap: true,
-    format: 'esm',
-    target: ['es2015'],
-    tsconfig: './tsconfig.json',
-    platform: 'node',
-    inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
-    define: {
-      global: 'global',
-      process: 'process',
-      Buffer: 'Buffer',
-      'process.env.NODE_ENV': `"production"`
-    },
-    plugins: [svgrPlugin(), plugin(stdLibBrowser), nodeExternalsPlugin()]
-  })
-  .catch(() => process.exit(1));
+  esbuild
+    .build({
+      entryPoints: files,
+      outdir: 'dist',
+      bundle: true,
+      treeShaking: true,
+      minify: true,
+      sourcemap: true,
+      format: 'esm',
+      target: ['es2015'],
+      tsconfig: './tsconfig.json',
+      platform: 'node',
+      inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
+      define: {
+        global: 'global',
+        process: 'process',
+        Buffer: 'Buffer',
+        'process.env.NODE_ENV': `"production"`
+      },
+      plugins: [svgrPlugin(), plugin(stdLibBrowser), nodeExternalsPlugin()]
+    })
+    .catch(() => process.exit(1));
+});
