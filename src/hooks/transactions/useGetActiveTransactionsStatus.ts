a048a4439ebd0bb5ac335837c948d10f1a1d3572
Stanislav Sava <sava.stanislav@gmail.com>
2022-06-23 15:07:58 +0300

    remove hasPendingTransactions flag

--- src/hooks/transactions/useGetActiveTransactionsStatus.ts
+++ src/hooks/transactions/useGetActiveTransactionsStatus.ts
@@ -2,22 +2,19 @@ import { useSelector } from 'reduxStore/DappProviderContext';
 import {
   failedTransactionsSelector,
   pendingSignedTransactionsSelector,
-  signedTransactionsSelector,
   successfulTransactionsSelector,
   timedOutTransactionsSelector
 } from 'reduxStore/selectors';
 
 interface UseGetActiveTransactionsStatusReturnType {
-  pending: boolean;
   timedOut: boolean;
   fail: boolean;
   success: boolean;
-  hasActiveTransactions: boolean;
+  pending: boolean;
 }
 
 //this is a hook to be able to take advantage of memoization offered by useSelector
 export function useGetActiveTransactionsStatus(): UseGetActiveTransactionsStatusReturnType {
-  const signedTransactions = useSelector(signedTransactionsSelector);
   const timedOutTransactions = useSelector(timedOutTransactionsSelector);
   const failedTransactions = useSelector(failedTransactionsSelector);
   const successfulTransactions = useSelector(successfulTransactionsSelector);
@@ -36,12 +33,10 @@ export function useGetActiveTransactionsStatus(): UseGetActiveTransactionsStatus
     !fail &&
     Object.keys(successfulTransactions).length > 0;
 
-  const hasActiveTransactions = Object.keys(signedTransactions).length > 0;
   return {
     pending,
     timedOut,
     fail,
-    success,
-    hasActiveTransactions
+    success
   };
 }
