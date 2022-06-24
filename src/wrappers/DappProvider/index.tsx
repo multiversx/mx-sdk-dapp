e12eb7a43e44c142046bdc525d90f4a7a5ba597a
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-06-24 14:05:23 +0300

    remove console.log

--- src/wrappers/DappProvider/index.tsx
+++ src/wrappers/DappProvider/index.tsx
@@ -29,8 +29,6 @@ export const DappProvider = ({
   environment,
   customComponents
 }: DappProviderPropsType) => {
-  console.log('DappProvider');
-
   if (!environment) {
     //throw if the user tries to initialize the app without a valid environment
     throw new Error('missing environment flag');
