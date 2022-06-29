91c386909291ca81037b53b4974dc74ce540c872
Stanislav Sava <sava.stanislav@gmail.com>
2022-06-29 16:20:00 +0300

    fix loginSessionMiddleware logout logic

--- src/redux/middlewares/loginSessionMiddleware.ts
+++ src/redux/middlewares/loginSessionMiddleware.ts
@@ -1,12 +1,9 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import throttle from 'lodash.throttle';
-import { getAccountProvider } from 'providers/accountProvider';
-import { logoutAction } from 'redux/commonActions';
-import { isLoggedInSelector } from 'redux/selectors/loginInfoSelectors';
-import { StoreType } from 'redux/store';
 import { getNewLoginExpiresTimestamp, setLoginExpiresAt } from 'storage/local';
 import storage from 'utils/storage';
 import { localStorageKeys } from 'utils/storage/local';
+import { logout } from 'utils';
 
 const whitelistedActions = ['logout'];
 
@@ -14,7 +11,7 @@ const throttledSetNewExpires = throttle(() => {
   setLoginExpiresAt(getNewLoginExpiresTimestamp());
 }, 5000);
 
-export const loginSessionMiddleware: any = (store: StoreType) => (
+export const loginSessionMiddleware: any = (store: any) => (
   next: (action: PayloadAction) => void
 ) => (action: PayloadAction) => {
   if (whitelistedActions.includes(action.type)) {
@@ -22,7 +19,7 @@ export const loginSessionMiddleware: any = (store: StoreType) => (
   }
   const appState = store.getState();
   const loginTimestamp = storage.local.getItem(localStorageKeys.loginExpiresAt);
-  const isLoggedIn = isLoggedInSelector(appState);
+  const isLoggedIn = appState?.loginInfo?.isLoggedIn;
   if (!isLoggedIn) {
     return next(action);
   }
@@ -33,11 +30,9 @@ export const loginSessionMiddleware: any = (store: StoreType) => (
   const isExpired = loginTimestamp - now < 0;
   if (isExpired) {
     return setTimeout(async () => {
-      const provider = getAccountProvider();
       console.log('session expired');
-      store.dispatch(logoutAction());
       try {
-        await provider?.logout({ callbackUrl: '/' });
+        logout();
       } catch (err) {
         console.error('error logging out', err);
       }
