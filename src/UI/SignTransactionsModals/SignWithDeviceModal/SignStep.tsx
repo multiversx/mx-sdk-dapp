f6ebae66b4860a87827d5826cbe0f65b0d948d1d
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-07-11 15:17:57 +0300

    small refactoring on SignStep component

--- src/UI/SignTransactionsModals/SignWithDeviceModal/SignStep.tsx
+++ src/UI/SignTransactionsModals/SignWithDeviceModal/SignStep.tsx
@@ -8,16 +8,14 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGetNetworkConfig } from 'hooks';
 import useGetTokenDetails from 'hooks/transactions/useGetTokenDetails';
-
 import { ActiveLedgerTransactionType, MultiSignTxType } from 'types';
 import { PageState } from 'UI/PageState';
 import { ProgressSteps } from 'UI/ProgressSteps';
 import { TokenDetails } from 'UI/TokenDetails';
 import { TransactionData } from 'UI/TransactionData';
-import { getGeneratedClasses } from 'UI/utils';
 import { denominate, getEgldLabel, isTokenTransfer } from 'utils';
 import globalStyles from 'assets/sass/main.scss';
-import classNames from 'classnames';
+import { useSignStepsClasses } from './hooks/useSignStepsClasses';
 
 export interface SignStepType {
   onSignTransaction: () => void;
@@ -107,72 +105,7 @@ export const SignStep = ({
   const scamReport = currentTransaction.receiverScamInfo;
   const showProgressSteps = allTransactions.length > 1;
 
-  const classes = getGeneratedClasses(className, true, {
-    formGroup: classNames(
-      globalStyles.formGroup,
-      globalStyles.textBreak,
-      globalStyles.textLeft
-    ),
-    formLabel: classNames(globalStyles.textBreak, globalStyles.textSecondary),
-    icon: globalStyles.textWhite,
-    contentWrapper: classNames(
-      globalStyles.dFlex,
-      globalStyles.flexColumn,
-      globalStyles.justifyContentStart,
-      globalStyles.flexRow,
-      globalStyles.justifyContentBetween,
-      globalStyles.mb3
-    ),
-    tokenWrapper: classNames(
-      globalStyles.mb3,
-      globalStyles.mb0,
-      globalStyles.dFlex,
-      globalStyles.flexColumn,
-      globalStyles.alignItemsStart
-    ),
-    tokenLabel: classNames(globalStyles.textSecondary, globalStyles.textLeft),
-    tokenValue: classNames(
-      globalStyles.dFlex,
-      globalStyles.alignItemsCenter,
-      globalStyles.mt1
-    ),
-    scamReport: globalStyles.textWarning,
-    scamReportIcon: classNames(globalStyles.textWarning, globalStyles.mr1),
-    tokenAmountLabel: classNames(
-      globalStyles.textSecondary,
-      globalStyles.textLeft
-    ),
-    tokenAmountValue: classNames(
-      globalStyles.dFlex,
-      globalStyles.alignItemsCenter
-    ),
-    dataFormGroup: classNames(globalStyles.formGroup, globalStyles.textLeft),
-    errorMessage: classNames(
-      globalStyles.textDanger,
-      globalStyles.dFlex,
-      globalStyles.justifyContentCenter,
-      globalStyles.alignItemsCenter
-    ),
-    buttonsWrapper: classNames(
-      globalStyles.dFlex,
-      globalStyles.alignItemsCenter,
-      globalStyles.justifyContentEnd,
-      globalStyles.mt1
-    ),
-    cancelButton: classNames(
-      globalStyles.btn,
-      globalStyles.btnDark,
-      globalStyles.textWhite,
-      globalStyles.dFlex,
-      globalStyles.mr2
-    ),
-    signButton: classNames(
-      globalStyles.btn,
-      scamReport ? globalStyles.btnWarning : globalStyles.btnPrimary,
-      globalStyles.dFlex,
-      globalStyles.ml2
-    )
-  });
+  const classes = useSignStepsClasses(className, scamReport);
 
   return (
     <PageState
