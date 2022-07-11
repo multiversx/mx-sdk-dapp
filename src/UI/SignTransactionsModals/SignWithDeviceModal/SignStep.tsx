d46f06db143e7ecdd569d47cc859a64bfe61ca0c
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-07-11 12:24:31 +0300

    small refactoring

--- src/UI/SignTransactionsModals/SignWithDeviceModal/SignStep.tsx
+++ src/UI/SignTransactionsModals/SignWithDeviceModal/SignStep.tsx
@@ -108,50 +108,64 @@ export const SignStep = ({
   const showProgressSteps = allTransactions.length > 1;
 
   const classes = getGeneratedClasses(className, true, {
-    formGroup: `${globalStyles.formGroup} ${globalStyles.textBreak} ${globalStyles.textLeft}`,
-    formLabel: `${globalStyles.textBreak} ${globalStyles.textSecondary}`,
+    formGroup: classNames(
+      globalStyles.formGroup,
+      globalStyles.textBreak,
+      globalStyles.textLeft
+    ),
+    formLabel: classNames(globalStyles.textBreak, globalStyles.textSecondary),
     icon: globalStyles.textWhite,
-    contentWrapper: `
-        ${globalStyles.dFlex}
-        ${globalStyles.flexColumn}
-        ${globalStyles.justifyContentStart}
-        ${globalStyles.flexRow}
-        ${globalStyles.justifyContentBetween}
-        ${globalStyles.mb3}
-      `,
-    tokenWrapper: `
-        ${globalStyles.mb3}
-        ${globalStyles.mb0}
-        ${globalStyles.dFlex}
-        ${globalStyles.flexColumn}
-        ${globalStyles.alignItemsStart}
-      `,
-    tokenLabel: `${globalStyles.textSecondary} ${globalStyles.textLeft}`,
-    tokenValue: `${globalStyles.dFlex} ${globalStyles.alignItemsCenter} ${globalStyles.mt1}`,
-    scamReport: `${globalStyles.textWarning}`,
-    scamReportIcon: `${globalStyles.textWarning} ${globalStyles.mr1}`,
-    tokenAmountLabel: `${globalStyles.textSecondary} ${globalStyles.textLeft}`,
-    tokenAmountValue: `${globalStyles.dFlex} ${globalStyles.alignItemsCenter}`,
-    dataFormGroup: `${globalStyles.formGroup} ${globalStyles.textLeft}`,
-    errorMessage: `
-        ${globalStyles.textDanger}
-        ${globalStyles.dFlex}
-        ${globalStyles.justifyContentCenter}
-        ${globalStyles.alignItemsCenter}
-      `,
-    buttonsWrapper: `
-        ${globalStyles.dFlex}
-        ${globalStyles.alignItemsCenter}
-        ${globalStyles.justifyContentEnd}
-        ${globalStyles.mt1}
-      `,
-    cancelButton: `
-        ${globalStyles.btn}
-        ${globalStyles.btnDark}
-        ${globalStyles.textWhite}
-        ${globalStyles.dFlex}
-        ${globalStyles.mr2}
-      `,
+    contentWrapper: classNames(
+      globalStyles.dFlex,
+      globalStyles.flexColumn,
+      globalStyles.justifyContentStart,
+      globalStyles.flexRow,
+      globalStyles.justifyContentBetween,
+      globalStyles.mb3
+    ),
+    tokenWrapper: classNames(
+      globalStyles.mb3,
+      globalStyles.mb0,
+      globalStyles.dFlex,
+      globalStyles.flexColumn,
+      globalStyles.alignItemsStart
+    ),
+    tokenLabel: classNames(globalStyles.textSecondary, globalStyles.textLeft),
+    tokenValue: classNames(
+      globalStyles.dFlex,
+      globalStyles.alignItemsCenter,
+      globalStyles.mt1
+    ),
+    scamReport: globalStyles.textWarning,
+    scamReportIcon: classNames(globalStyles.textWarning, globalStyles.mr1),
+    tokenAmountLabel: classNames(
+      globalStyles.textSecondary,
+      globalStyles.textLeft
+    ),
+    tokenAmountValue: classNames(
+      globalStyles.dFlex,
+      globalStyles.alignItemsCenter
+    ),
+    dataFormGroup: classNames(globalStyles.formGroup, globalStyles.textLeft),
+    errorMessage: classNames(
+      globalStyles.textDanger,
+      globalStyles.dFlex,
+      globalStyles.justifyContentCenter,
+      globalStyles.alignItemsCenter
+    ),
+    buttonsWrapper: classNames(
+      globalStyles.dFlex,
+      globalStyles.alignItemsCenter,
+      globalStyles.justifyContentEnd,
+      globalStyles.mt1
+    ),
+    cancelButton: classNames(
+      globalStyles.btn,
+      globalStyles.btnDark,
+      globalStyles.textWhite,
+      globalStyles.dFlex,
+      globalStyles.mr2
+    ),
     signButton: classNames(
       globalStyles.btn,
       scamReport ? globalStyles.btnWarning : globalStyles.btnPrimary,
