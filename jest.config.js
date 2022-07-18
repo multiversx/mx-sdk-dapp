9efda89e7ed3a9601d3ce95f439ea66eb13541dd
Grigore Budac <contact@grigorebudac.com>
2022-07-18 16:26:45 +0200

    fix scss transform

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,11 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.[t|j]sx?$': 'ts-jest'
+    '^.+\\.[t|j]sx?$': 'ts-jest',
+    '^.+\\.scss$': 'jest-scss-transform'
   },
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'identity-obj-proxy'
+    '.+\\.(css|styl|less|sass|png|jpg|ttf|woff|woff2)$': 'identity-obj-proxy'
   },
   moduleNameMapper: {
     uint8arrays: '<rootDir>/node_modules/uint8arrays/cjs/src',
