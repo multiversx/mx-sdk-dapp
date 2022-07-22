8a0fa30c4ed91fdb00ad5335ff71359b112cfb59
Tudor Morar <tudor.morar@elrond.com>
2022-07-22 17:06:23 +0300

    Work on tests

--- executeBuildCommand.js
+++ executeBuildCommand.js
@@ -72,6 +72,12 @@ module.exports = function esbuildWrapper(buildType = 'esm') {
           ],
           ...customOptions
         })
+        .then(() => {
+          console.log(
+            '\x1b[36m%s\x1b[0m',
+            `[${new Date().toLocaleTimeString()}] Dapp-core build succeeded for ${format} types`
+          );
+        })
         .catch(() => process.exit(1));
     });
   };
