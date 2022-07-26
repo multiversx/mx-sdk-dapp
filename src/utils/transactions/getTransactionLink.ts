aecf8f1259338a37e53496c7f5e0a48c748ae9e5
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-07-26 10:04:53 +0300

    add getAllTransactionsLink utils function

--- src/utils/transactions/getTransactionLink.ts
+++ src/utils/transactions/getTransactionLink.ts
@@ -1,5 +1,12 @@
 import { NetworkType } from 'types';
 
-export function getTransactionLink(network: NetworkType, address: string) {
+export function getTransactionLink(
+  network: NetworkType,
+  transactionHash: string
+) {
+  return `${network.explorerAddress}/transactions/${transactionHash}`;
+}
+
+export function getAllTransactionsLink(network: NetworkType, address: string) {
   return `${network.explorerAddress}/address/${address}`;
 }
