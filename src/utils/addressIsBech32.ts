6eb18a2f67938248497cd8d9ff7a1469206591a6
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-02 12:52:09 +0300

    remove addressIsBetch32 because as duplicate of addressIsValid

--- src/utils/addressIsBech32.ts
+++
@@ -1,9 +0,0 @@
-export function addressIsBech32(destinationAddress = '') {
-  const isValidBach = !(
-    !destinationAddress ||
-    !destinationAddress.startsWith('erd') ||
-    destinationAddress.length !== 62 ||
-    /^\w+$/.test(destinationAddress) !== true
-  );
-  return isValidBach;
-}
