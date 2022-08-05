87aa4b62aed1440f5547327d3517a34e1f9dbd38
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-05 13:45:37 +0300

    fix build

--- src/components/TransactionsInterpreter/helpers/getOperationsMessages.ts
+++ src/components/TransactionsInterpreter/helpers/getOperationsMessages.ts
@@ -1,7 +1,9 @@
 import { TransactionType } from './types';
 
 export function getOperationsMessages(transaction: TransactionType) {
-  return transaction?.operations
-    ?.map((operation) => operation.message)
-    .filter((messages): messages is string => Boolean(messages));
+  return (
+    transaction?.operations
+      ?.map((operation) => operation.message)
+      .filter((messages): messages is string => Boolean(messages)) ?? []
+  );
 }
