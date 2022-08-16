df13f1a065b842522b26b164c19cfdd1288c28ab
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-16 18:25:39 +0300

    missing change

--- src/components/TransactionsTracker/useClosureRef.ts
+++ src/components/TransactionsTracker/useClosureRef.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 
 export const useClosureRef = <T>(value: T) => {
-  const refVal = useRef(value);
+  const refVal = useRef<T>(value);
 
   useEffect(() => {
     refVal.current = value;
