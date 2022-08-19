2aa7fb88eca75e2a144d7e80e16c2f273353fe78
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-20 00:22:09 +0300

    add tests for getTransactionTransferType

---
+++ src/components/TransactionsInterpreter/helpers/tests/getTransactionTransferType.test.ts
@@ -0,0 +1,63 @@
+import { getTransactionTransferType } from '../getTransactionTransferType';
+import { TransactionDirection, TransferType, TransferTypeEnum } from '../types';
+import { transactionMock } from './transaction-mock';
+
+describe('getTransactionTransferType', () => {
+  it(`returns "${TransactionDirection.INTERNAL}" when the transaction type is "${TransferTypeEnum.SmartContractResult}" `, () => {
+    const transaction: TransferType = {
+      ...transactionMock,
+      type: TransferTypeEnum.SmartContractResult
+    };
+
+    const result = getTransactionTransferType(
+      'address-hash',
+      transaction,
+      'receiver-hash'
+    );
+
+    expect(result).toEqual(TransactionDirection.INTERNAL);
+  });
+
+  it(`returns "${TransactionDirection.SELF}" for self transfers`, () => {
+    const sender = 'my-address-hash';
+
+    const transaction: TransferType = {
+      ...transactionMock,
+      sender
+    };
+
+    const result = getTransactionTransferType(sender, transaction, sender);
+
+    expect(result).toEqual(TransactionDirection.SELF);
+  });
+
+  it(`returns "${TransactionDirection.IN}" when receive something from an address`, () => {
+    const receiver = 'my-address-hash';
+
+    const transaction: TransferType = {
+      ...transactionMock,
+      receiver
+    };
+
+    const result = getTransactionTransferType(receiver, transaction, receiver);
+
+    expect(result).toEqual(TransactionDirection.IN);
+  });
+
+  it(`returns "${TransactionDirection.OUT}" for when transfer something to an address`, () => {
+    const sender = 'my-address-hash';
+
+    const transaction: TransferType = {
+      ...transactionMock,
+      sender
+    };
+
+    const result = getTransactionTransferType(
+      sender,
+      transaction,
+      'receiver-hash'
+    );
+
+    expect(result).toEqual(TransactionDirection.OUT);
+  });
+});
