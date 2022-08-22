288add9391fe29060c89283d4e405e47e5aaa02f
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-22 10:38:52 +0300

    change base transaction mock signature

--- src/components/TransactionsInterpreter/helpers/tests/base-transaction-mock.ts
+++ src/components/TransactionsInterpreter/helpers/tests/base-transaction-mock.ts
@@ -15,8 +15,7 @@ export const baseTransactionMock = {
   round: 2044988,
   sender: 'erd1wh9c0sjr2xn8hzf02lwwcr4jk2s84tat9ud2kaq6zr7xzpvl9l5q8awmex',
   senderShard: 0,
-  signature:
-    'ef74ee8d37dbacaf54a80cf6e5d15d1412f1b88c8dbeb0e0c1e33f97f4fadaf402a71c94a3ae42ee4d7320dd72bd800ddf8c69946adb495196662e4f985f6003',
+  signature: 'transaction-signature-hash',
   status: 'fail',
   value: '1234',
   fee: '655440000000000',
