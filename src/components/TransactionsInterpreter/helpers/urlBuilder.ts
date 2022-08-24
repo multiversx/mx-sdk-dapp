ec551bd9085cd2b5df2ce7b37eca62b5ac48ddc7
Tudor Morar <tudor.morar@elrond.com>
2022-08-24 11:16:05 +0300

    Update endpoints

--- src/components/TransactionsInterpreter/helpers/urlBuilder.ts
+++ src/components/TransactionsInterpreter/helpers/urlBuilder.ts
@@ -1,30 +1,51 @@
+import {
+  ACCOUNTS_ENDPOINT,
+  BLOCKS_ENDPOINT,
+  COLLECTIONS_ENDPOINT,
+  IDENTITIES_ENDPOINT,
+  NFTS_ENDPOINT,
+  NODES_ENDPOINT,
+  PROVIDERS_ENDPOINT,
+  TOKENS_ENDPOINT,
+  TRANSACTIONS_ENDPOINT
+} from 'apiCalls/endpoints';
+
 export const urlBuilder = {
-  shard: (shard: number | string) => `/blocks?shard=${shard}`,
+  shard: (shard: number | string) => `/${BLOCKS_ENDPOINT}?shard=${shard}`,
   receiverShard: (shard: number | string) =>
-    `/transactions?receivershard=${shard}`,
-  senderShard: (shard: number | string) => `/transactions?sendershard=${shard}`,
-  transactionDetails: (hash: number | string) => `/transactions/${hash}`,
+    `/${TRANSACTIONS_ENDPOINT}?receivershard=${shard}`,
+  senderShard: (shard: number | string) =>
+    `/${TRANSACTIONS_ENDPOINT}?sendershard=${shard}`,
+  transactionDetails: (hash: number | string) =>
+    `/${TRANSACTIONS_ENDPOINT}/${hash}`,
   transactionDetailsScResults: (hash: string) =>
-    `/transactions/${hash}/sc-results`,
-  transactionDetailsLogs: (hash: string) => `/transactions/${hash}/logs`,
-  nodeDetails: (publicKey: string) => `/nodes/${publicKey}`,
-  accountDetails: (address: string) => `/accounts/${address}`,
-  accountDetailsContractCode: (address: string) => `/accounts/${address}/code`,
-  accountDetailsTokens: (address: string) => `/accounts/${address}/tokens`,
-  accountDetailsNfts: (address: string) => `/accounts/${address}/nfts`,
+    `/${TRANSACTIONS_ENDPOINT}/${hash}/sc-results`,
+  transactionDetailsLogs: (hash: string) =>
+    `/${TRANSACTIONS_ENDPOINT}/${hash}/logs`,
+  nodeDetails: (publicKey: string) => `/${NODES_ENDPOINT}/${publicKey}`,
+  accountDetails: (address: string) => `/${ACCOUNTS_ENDPOINT}/${address}`,
+  accountDetailsContractCode: (address: string) =>
+    `/${ACCOUNTS_ENDPOINT}/${address}/code`,
+  accountDetailsTokens: (address: string) =>
+    `/${ACCOUNTS_ENDPOINT}/${address}/${TOKENS_ENDPOINT}`,
+  accountDetailsNfts: (address: string) =>
+    `/${ACCOUNTS_ENDPOINT}/${address}/nfts`,
   accountDetailsScResults: (address: string) =>
-    `/accounts/${address}/sc-results`,
+    `/${ACCOUNTS_ENDPOINT}/${address}/sc-results`,
   accountDetailsContracts: (address: string) =>
-    `/accounts/${address}/contracts`,
-  identityDetails: (id: string) => `/identities/${id}`,
-  tokenDetails: (tokenId: string) => `/tokens/${tokenId}`,
-  tokenDetailsAccounts: (tokenId: string) => `/tokens/${tokenId}/accounts`,
+    `/${ACCOUNTS_ENDPOINT}/${address}/contracts`,
+  identityDetails: (id: string) => `/${IDENTITIES_ENDPOINT}/${id}`,
+  tokenDetails: (tokenId: string) => `/${TOKENS_ENDPOINT}/${tokenId}`,
+  tokenDetailsAccounts: (tokenId: string) =>
+    `/${TOKENS_ENDPOINT}/${tokenId}/${ACCOUNTS_ENDPOINT}`,
   tokenDetailsLockedAccounts: (tokenId: string) =>
-    `/tokens/${tokenId}/locked-accounts`,
-  tokenDetailsRoles: (tokenId: string) => `/tokens/${tokenId}/roles`,
-  collectionDetails: (identifier: string) => `/collections/${identifier}`,
-  nftDetails: (identifier: string) => `/nfts/${identifier}`,
-  providerDetails: (address: string) => `/providers/${address}`,
+    `/${TOKENS_ENDPOINT}/${tokenId}/locked-accounts`,
+  tokenDetailsRoles: (tokenId: string) =>
+    `/${TOKENS_ENDPOINT}/${tokenId}/roles`,
+  collectionDetails: (identifier: string) =>
+    `/${COLLECTIONS_ENDPOINT}/${identifier}`,
+  nftDetails: (identifier: string) => `/${NFTS_ENDPOINT}/${identifier}`,
+  providerDetails: (address: string) => `/${PROVIDERS_ENDPOINT}/${address}`,
   providerDetailsTransactions: (address: string) =>
-    `/providers/${address}/transactions`
+    `/${PROVIDERS_ENDPOINT}/${address}/${TRANSACTIONS_ENDPOINT}`
 };
