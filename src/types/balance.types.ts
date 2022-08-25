2912d7f0fa07b8255cd6c57b4cbd1996c546df2f
Stanislav Sava <sava.stanislav@gmail.com>
2022-08-25 14:54:13 +0300

    update type to interface

--- src/types/balance.types.ts
+++ src/types/balance.types.ts
@@ -1,8 +1,8 @@
-export type FormatAmountType = {
+export interface FormatAmountType {
   input: string;
   decimals?: number;
   digits?: number;
   showIsLessThanDecimalsLabel?: boolean;
   showLastNonZeroDecimal?: boolean;
   addCommas?: boolean;
-};
+}
