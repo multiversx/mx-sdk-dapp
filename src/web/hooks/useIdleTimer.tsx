21d43657ade1afd2a6841dd900f622b01bd10d47
Ciprian Draghici <cipriandraghici93@gmail.com>
2022-08-25 10:37:18 +0300

    fix imports

--- src/web/hooks/useIdleTimer.tsx
+++ src/web/hooks/useIdleTimer.tsx
@@ -1,7 +1,7 @@
 import { useIdleTimer as useReactIdleTimer } from 'react-idle-timer';
 import { useClosureRef } from 'components/TransactionsTracker/useClosureRef';
-import { useGetIsLoggedIn } from 'hooks';
-import { logout as dappLogout } from 'utils';
+import { useGetIsLoggedIn } from 'hooks/account/useGetIsLoggedIn';
+import { logout as dappLogout } from 'utils/logout';
 
 const getTimeout = (minutes: number) => 1000 * 60 * minutes;
 const debounce = 500;
