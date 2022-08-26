b5cf2c23a69775e4f28c022323c6d231373d5029
Tudor Morar <tudor.morar@elrond.com>
2022-08-26 17:29:02 +0300

    Add count and transaction to export

--- src/apiCalls/transactions/index.ts
+++ src/apiCalls/transactions/index.ts
@@ -1,4 +1,6 @@
 export * from './sendSignedTransactions';
 export * from './getTransactionsByHashes';
 export * from './getTransactions';
+export * from './getTransactionsCount';
+export * from './getTransaction';
 export * from './getTransactions.types';
