119564373219fcc349a66b2a47b3a5a9c8c630cf
Tudor Morar <tudor.morar@elrond.com>
2022-08-31 15:56:09 +0300

    Revert account hooks

--- src/hooks/account/useGetAccount.ts
+++ src/hooks/account/useGetAccount.ts
@@ -1,11 +1,6 @@
-import { useMemo } from 'react';
 import { useSelector } from 'reduxStore/DappProviderContext';
 import { accountSelector } from 'reduxStore/selectors';
 
 export const useGetAccount = () => {
-  const account = useSelector(accountSelector);
-  const memoizedAccount = useMemo(() => {
-    return account;
-  }, [account]);
-  return memoizedAccount;
+  return useSelector(accountSelector);
 };
--- src/hooks/account/useGetAccountInfo.ts
+++ src/hooks/account/useGetAccountInfo.ts
@@ -1,18 +1,6 @@
-import { useMemo } from 'react';
 import { useSelector } from 'reduxStore/DappProviderContext';
 import { accountInfoSelector } from 'reduxStore/selectors';
-import { useGetAccount } from './useGetAccount';
 
 export const useGetAccountInfo = () => {
-  const account = useGetAccount();
-  const info = useSelector(accountInfoSelector);
-
-  const memoizedAccountInfo = useMemo(() => {
-    return {
-      ...info,
-      account
-    };
-  }, [account, info]);
-
-  return memoizedAccountInfo;
+  return useSelector(accountInfoSelector);
 };
