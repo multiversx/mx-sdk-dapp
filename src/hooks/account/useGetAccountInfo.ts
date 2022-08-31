ae14a2d5c69523656e760c7bbbf648d4f8228433
Tudor Morar <tudor.morar@elrond.com>
2022-08-31 14:33:41 +0300

    Memoized account selectors

--- src/hooks/account/useGetAccountInfo.ts
+++ src/hooks/account/useGetAccountInfo.ts
@@ -1,6 +1,18 @@
+import { useMemo } from 'react';
 import { useSelector } from 'reduxStore/DappProviderContext';
 import { accountInfoSelector } from 'reduxStore/selectors';
+import { useGetAccount } from './useGetAccount';
 
 export const useGetAccountInfo = () => {
-  return useSelector(accountInfoSelector);
+  const account = useGetAccount();
+  const info = useSelector(accountInfoSelector);
+
+  const accountInfo = useMemo(() => {
+    return {
+      ...info,
+      account
+    };
+  }, [account, info]);
+
+  return accountInfo;
 };
