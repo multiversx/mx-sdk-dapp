116796b56e6b0d09274ed1d2bb27866a83951f5e
Tudor Morar <tudor.morar@elrond.com>
2022-09-05 11:55:22 +0300

    Remove async keyword from tests

--- src/wrappers/AuthenticatedRoutesWrapper/helpers/tests/matchRoute.test.ts
+++ src/wrappers/AuthenticatedRoutesWrapper/helpers/tests/matchRoute.test.ts
@@ -9,22 +9,22 @@ const createRoutes = (path: string, authenticatedRoute = true) => [
 ];
 
 describe('matchRoute', () => {
-  it('should return true for simple routes', async () => {
+  it('should return true for simple routes', () => {
     const result = matchRoute(createRoutes('/home'), '/home');
     expect(result).toBe(true);
   });
-  it('should return true for pattern routes', async () => {
+  it('should return true for pattern routes', () => {
     const result = matchRoute(createRoutes('/user/:id'), '/user/first-name');
     expect(result).toBe(true);
   });
-  it('should return false for non-matching pattern routes', async () => {
+  it('should return false for non-matching pattern routes', () => {
     const result = matchRoute(
       createRoutes('/user/:id'),
       '/user/first-name/detail'
     );
     expect(result).toBe(false);
   });
-  it('should return true for non-athenticated non-matching pattern routes', async () => {
+  it('should return true for non-athenticated non-matching pattern routes', () => {
     const result = matchRoute(
       createRoutes('/user/:id', false),
       '/user/first-name'
