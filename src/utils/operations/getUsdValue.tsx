b6857c2dc52a0648028fb09fbda6048eaf6c58fd
Tudor Morar <tudor.morar@elrond.com>
2022-09-14 15:37:23 +0300

    Allow equal sign configuration

--- src/utils/operations/getUsdValue.tsx
+++ src/utils/operations/getUsdValue.tsx
@@ -2,14 +2,21 @@ export interface UsdValueType {
   amount: string;
   usd: number;
   decimals?: number;
+  addEqualSign?: boolean;
 }
 
-export const getUsdValue = ({ amount, usd, decimals = 2 }: UsdValueType) => {
+export const getUsdValue = ({
+  amount,
+  usd,
+  decimals = 2,
+  addEqualSign
+}: UsdValueType) => {
   const sum = (parseFloat(amount) * usd).toFixed(decimals);
   const formattedValue = parseFloat(sum).toLocaleString('en', {
     maximumFractionDigits: decimals,
     minimumFractionDigits: decimals
   });
   const equalSign = parseFloat(amount) > 0 ? '≈' : '=';
-  return `${equalSign} $${formattedValue}`;
+  const equalSignPrefix = addEqualSign ? `${equalSign} ` : '';
+  return `${equalSignPrefix}$${formattedValue}`;
 };
