28bbe456ef1fa7ac83ee57b62643a2b74f08a3fb
Tudor Morar <tudor.morar@elrond.com>
2022-09-19 11:37:46 +0300

    Add TransactionMethod test

--- src/UI/TransactionsTable/components/TransactionMethod.tsx
+++ src/UI/TransactionsTable/components/TransactionMethod.tsx
@@ -19,6 +19,7 @@ export const TransactionMethod = ({
         globalStyles.badgePill,
         globalStyles.fontWeightLight
       )}
+      data-testid='transactionMethod'
     >
       <div
         className={classNames(
