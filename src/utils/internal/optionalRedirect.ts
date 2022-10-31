74944e82bb9e7b15988a8a1e3897da6e421dad66
Tudor Morar <tudor.morar@elrond.com>
2022-10-31 17:15:36 +0300

    [+] Always execute onLoginRedirect if defined

--- src/utils/internal/optionalRedirect.ts
+++ src/utils/internal/optionalRedirect.ts
@@ -1,20 +1,26 @@
 import { safeRedirect } from '../redirect';
 
+const DEFAULT_TIMEOUT = 200;
+
 export function optionalRedirect(
   callbackUrl?: string,
   onLoginRedirect?: (callbackRoute: string) => void
 ) {
   const shouldRedirect = Boolean(callbackUrl);
 
+  const hasOnLoginRedirect = typeof onLoginRedirect === 'function';
+
+  const timeout = hasOnLoginRedirect ? 0 : DEFAULT_TIMEOUT;
+
   if (shouldRedirect && callbackUrl != null) {
     setTimeout(() => {
-      if (!window.location.pathname.includes(callbackUrl)) {
-        if (typeof onLoginRedirect === 'function') {
-          onLoginRedirect(callbackUrl);
-        } else {
-          safeRedirect(callbackUrl);
-        }
+      // if onLoginRedirect is defined, it has priority over safeRedirect
+      if (hasOnLoginRedirect) {
+        return onLoginRedirect(callbackUrl);
       }
-    }, 200);
+      if (!window.location.pathname.includes(callbackUrl)) {
+        safeRedirect(callbackUrl);
+      }
+    }, timeout);
   }
 }
