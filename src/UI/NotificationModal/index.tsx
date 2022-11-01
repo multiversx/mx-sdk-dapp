529d467ac86d282e90fc3a5c863ed2e06f08ebf8
Tudor Morar <tudor.morar@elrond.com>
2022-11-01 12:35:31 +0300

    [*] Make notification only show up if content is present

--- src/UI/NotificationModal/index.tsx
+++ src/UI/NotificationModal/index.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames';
 
+import globalStyles from 'assets/sass/main.scss';
 import { useGetNotification } from 'hooks';
 import { NotificationTypesEnum } from 'types';
 import { ModalContainer } from '../ModalContainer';
 import { PageState } from '../PageState';
-
-import globalStyles from 'assets/sass/main.scss';
 
 const notificationTypesToIcons = {
   [NotificationTypesEnum.warning]: faExclamationTriangle
@@ -26,24 +25,33 @@ export function NotificationModal() {
     ? notificationTypesToIcons[type] || defaultIcon
     : null;
 
+  if (!notification) {
+    return null;
+  }
+
   return (
-    <ModalContainer onClose={clearNotification} visible={Boolean(notification)}>
-      <PageState
-        icon={icon}
-        iconClass={notification?.iconClassName}
-        iconBgClass={classNames(globalStyles.p4, globalStyles.roundedBgCircle)}
-        iconSize='3x'
-        title={notification?.title}
-        description={notification?.description}
-        action={
-          <button
-            className={classNames(globalStyles.btn, globalStyles.btnPrimary)}
-            onClick={onDone}
-          >
-            Done
-          </button>
-        }
-      />
+    <ModalContainer onClose={clearNotification} visible>
+      <>
+        <PageState
+          icon={icon}
+          iconClass={notification?.iconClassName}
+          iconBgClass={classNames(
+            globalStyles.p4,
+            globalStyles.roundedBgCircle
+          )}
+          iconSize='3x'
+          title={notification?.title}
+          description={notification?.description}
+          action={
+            <button
+              className={classNames(globalStyles.btn, globalStyles.btnPrimary)}
+              onClick={onDone}
+            >
+              Done
+            </button>
+          }
+        />
+      </>
     </ModalContainer>
   );
 }
