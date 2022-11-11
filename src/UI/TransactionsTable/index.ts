5a841cd96178c0b185a43677a7db199972aa6216
Razvan Tomegea <razvan.tomegea@gmail.com>
2022-11-11 09:03:43 +0200

    Transacation link helper

--- src/UI/TransactionsTable/index.ts
+++ src/UI/TransactionsTable/index.ts
@@ -4,3 +4,4 @@ export { TransactionRow } from './components/TransactionRow';
 export { TransactionSender } from './components/TransactionSender';
 export { TransactionShardsTransition } from './components/TransactionShardsTransition';
 export { TransactionsTable } from './TransactionsTable';
+export { TransactionValue } from './components/TransactionValue';
