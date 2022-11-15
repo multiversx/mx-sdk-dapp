d7c2a7cb3b35836251b0a65fd3c5f587e94e2a34
Tudor Morar <tudor.morar@elrond.com>
2022-11-15 15:08:33 +0300

    Refactor TransactionToast component

---
+++ src/UI/TransactionsToastList/components/TransactionToast/TransactionToast.tsx
@@ -0,0 +1,78 @@
+import React, { ReactNode } from 'react';
+
+import { Progress } from 'UI/Progress';
+
+import { WithClassnameType } from '../../../types';
+import { TransactionToastContent, TransactionToastWrapper } from './components';
+import { useTransactionToast } from './hooks/useTransactionToast';
+import styles from './transactionToast.styles.scss';
+import {
+  TransactionToastCustomizationProps,
+  TransactionToastDefaultProps
+} from './transactionToast.type';
+
+export interface TransactionToastPropsType
+  extends TransactionToastDefaultProps,
+    WithClassnameType {
+  title?: string | ReactNode;
+  children?: ReactNode;
+  customization?: TransactionToastCustomizationProps;
+}
+
+export const TransactionToast = ({
+  toastId,
+  title = '',
+  className = 'dapp-transaction-toast',
+  onDelete,
+  startTimestamp,
+  endTimeProgress,
+  lifetimeAfterSuccess,
+  status,
+  transactions,
+  customization = {
+    TransactionToastContent,
+    Progress
+  }
+}: TransactionToastPropsType) => {
+  const {
+    progress,
+    isPending,
+    isTimedOut,
+    toastDataState,
+    handleDeleteToast
+  } = useTransactionToast({
+    toastId,
+    transactions,
+    status,
+    lifetimeAfterSuccess,
+    startTimestamp,
+    endTimeProgress,
+    onDelete
+  });
+
+  const ProgressComponent = customization.Progress!;
+  const TransactionToastContentComponent = customization.TransactionToastContent!;
+
+  return (
+    <TransactionToastWrapper className={className}>
+      <ProgressComponent
+        key={toastId}
+        id={toastId}
+        progress={progress}
+        expiresIn={lifetimeAfterSuccess}
+        done={!isPending || isTimedOut}
+      >
+        <TransactionToastContentComponent
+          style={styles}
+          toastDataState={toastDataState}
+          transactions={transactions ?? []}
+          toastTitle={title}
+          isTimedOut={isTimedOut}
+          showCloseButton={!isPending}
+          onDeleteToast={handleDeleteToast}
+          customElements={customization?.TransactionToastContentCustomElements}
+        />
+      </ProgressComponent>
+    </TransactionToastWrapper>
+  );
+};
