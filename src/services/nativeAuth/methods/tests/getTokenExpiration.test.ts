f542cccbb6d01610ac1a766c8ec53949fae90795
Tudor Morar <tudor.morar@elrond.com>
2022-12-13 17:00:44 +0300

    Extend tests

--- src/services/nativeAuth/methods/tests/getTokenExpiration.test.ts
+++ src/services/nativeAuth/methods/tests/getTokenExpiration.test.ts
@@ -11,14 +11,18 @@ describe('getTokenExpiration tests', () => {
     const timestamp = 1669712400; // Tue Nov 29 2022 09:00:00 GMT+0000
     jest.spyOn(timestampModule, 'getUnixTimestamp').mockReturnValue(timestamp);
 
-    const { isExpired } = getTokenExpiration(nativeAuthToken);
-    expect(isExpired).toStrictEqual(true);
+    const data = getTokenExpiration(nativeAuthToken);
+    expect(data.isExpired).toStrictEqual(true);
+    expect(data.expiresAt).toStrictEqual(1669712220);
+    expect(data.secondsUntilExpires).toStrictEqual(-180);
   });
   it('checks expired false', () => {
     const timestamp = 1669712160; //Tue Nov 29 2022 08:56:00 GMT+0000
     jest.spyOn(timestampModule, 'getUnixTimestamp').mockReturnValue(timestamp);
 
-    const { isExpired } = getTokenExpiration(nativeAuthToken);
-    expect(isExpired).toStrictEqual(false);
+    const data = getTokenExpiration(nativeAuthToken);
+    expect(data.isExpired).toStrictEqual(false);
+    expect(data.expiresAt).toStrictEqual(1669712220);
+    expect(data.secondsUntilExpires).toStrictEqual(60);
   });
 });
