25f0b7a9df95520cd1726c9f45f3cc1cb64f7ead
Tudor Morar <tudor.morar@elrond.com>
2022-12-19 12:53:31 +0300

    Remove console.log

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -7,8 +7,6 @@ export const preventRedirects = (shouldPreventRedirect = true) => {
 export const safeRedirect = (url: string, timeout = 0) => {
   if (!preventRedirect) {
     setTimeout(() => {
-      console.log(11);
-
       window.location.href = url;
       window.location.assign(url);
     }, timeout);
