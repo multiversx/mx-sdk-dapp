ee2c384784156bb1ce779c5d9a3010e2a6235bc2
Tudor Morar <tudor.morar@elrond.com>
2022-12-19 12:23:58 +0300

    Prepare nativeAuth test

--- src/UI/extension/ExtensionLoginButton/tests/helpers/mockExtensionProvider.ts
+++ src/UI/extension/ExtensionLoginButton/tests/helpers/mockExtensionProvider.ts
@@ -2,14 +2,38 @@ import { testAddress } from '__mocks__';
 
 export const ExtensionProvider = {
   getInstance: () => {
+    let signature: string | undefined = undefined;
+
     const provider = {
       init: async () => true,
-      login: async () => true,
-      setAddress: () => provider,
-      account: {
-        address: testAddress
-      }
+      login: async ({ token }: { token?: string }) => {
+        if (token) {
+          signature =
+            'e4c98dd01020118b13db9dd5db9e5b56ff0c4a0141306918a9d3eea964a21ada5d566f58cdf6c921ed3405bf5685d1e87545dbcc86ea3c27a43aa3abee8c2b0e';
+        }
+        return {
+          address:
+            'erd1dm9uxpf5awkn7uhju7zjn9lde0dhahy0qaxqqlu26xcuuw27qqrsqfmej3'
+        };
+      },
+      setAddress: () => provider
     };
+
+    Object.defineProperty(provider, 'account', {
+      get() {
+        const account = {
+          address: testAddress
+        };
+        if (signature) {
+          return {
+            ...account,
+            signature
+          };
+        }
+        return account;
+      }
+    });
+
     return provider;
   }
 };
