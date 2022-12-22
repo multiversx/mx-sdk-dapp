a34e3fa54c2bef14d88c991276f232cf6a5ef772
Tudor Morar <tudor.morar@elrond.com>
2022-12-22 19:02:25 +0300

    Refactor

--- src/UI/extension/ExtensionLoginButton/tests/mocks/mockExtensionProvider.ts
+++ src/UI/extension/ExtensionLoginButton/tests/mocks/mockExtensionProvider.ts
@@ -12,8 +12,7 @@ export const ExtensionProvider = {
             'e4c98dd01020118b13db9dd5db9e5b56ff0c4a0141306918a9d3eea964a21ada5d566f58cdf6c921ed3405bf5685d1e87545dbcc86ea3c27a43aa3abee8c2b0e';
         }
         return Promise.resolve({
-          address:
-            'erd1dm9uxpf5awkn7uhju7zjn9lde0dhahy0qaxqqlu26xcuuw27qqrsqfmej3'
+          address: testAddress
         });
       },
       setAddress: () => provider,
