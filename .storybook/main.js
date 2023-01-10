12044903979ae778b6f865d42e5c62c189841f2f
cipriandraghici <cipriandraghici93@gmail.com>
2023-01-10 14:05:17 +0200

    fix alias for wallet-connect in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -20,7 +20,7 @@ module.exports = {
 
     // walletConnectV2Provider has compliation errors for storybook
     config.resolve.alias[
-      'elrondnetwork/erdjs-wallet-connect-provider/out/walletConnectV2Provider'
+      '@elrondnetwork/erdjs-wallet-connect-provider/out/walletConnectV2Provider'
     ] = require.resolve(
       '@elrondnetwork/erdjs-wallet-connect-provider/out/walletConnectProvider'
     );
