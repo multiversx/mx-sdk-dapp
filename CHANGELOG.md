dcc33fe507b3201be81b59c708d294818082b662
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2023-01-26 11:20:47 +0200

    Optional meridiem in `getHumanReadableTimeFormat` util function (#597)
    Co-authored-by: Tudor Morar <tudor.morar@elrond.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+- [Added support to not show meridiem by default in `getHumanReadableTimeFormat` util function](https://github.com/multiversx/mx-sdk-dapp/pull/597)
 - [Fixed web wallet cancelled login](https://github.com/multiversx/mx-sdk-dapp/pull/598)
 
 ## [[v2.3.0](https://github.com/multiversx/mx-sdk-dapp/pull/596)] - 2023-01-20
--- src/utils/transactions/getInterpretedTransaction/helpers/tests/getHumanReadableTimeFormat.test.ts
+++ src/utils/transactions/getInterpretedTransaction/helpers/tests/getHumanReadableTimeFormat.test.ts
@@ -64,4 +64,76 @@ describe('getHumanReadableTimeFormat', () => {
 
     expect(result).toEqual(`Apr 23, 1993 0${hours}:33 AM UTC`);
   });
+
+  it('returns full readable date, non-UTC, without meridiem', () => {
+    const date = new Date(1993, 3, 23, 8, 33, 12);
+    const value = date.getTime() / 1000; // UNIX timestamp
+    const hours = date.getUTCHours();
+    const noSeconds = false;
+    const utc = false;
+    const meridiem = false;
+
+    const result = getHumanReadableTimeFormat({
+      value,
+      noSeconds,
+      utc,
+      meridiem
+    });
+
+    expect(result).toEqual(`Apr 23, 1993 0${hours}:33:12`);
+  });
+
+  it('returns readable date without seconds, non-UTC, without meridiem', () => {
+    const date = new Date(1993, 3, 23, 8, 33, 12);
+    const value = date.getTime() / 1000; // UNIX timestamp
+    const hours = date.getUTCHours();
+    const noSeconds = true;
+    const utc = false;
+    const meridiem = false;
+
+    const result = getHumanReadableTimeFormat({
+      value,
+      noSeconds,
+      utc,
+      meridiem
+    });
+
+    expect(result).toEqual(`Apr 23, 1993 0${hours}:33`);
+  });
+
+  it('returns full readable date in UTC, without meridiem', () => {
+    const date = new Date(1993, 3, 23, 8, 33, 12);
+    const value = date.getTime() / 1000; // UNIX timestamp
+    const noSeconds = false;
+    const utc = true;
+    const hours = date.getUTCHours();
+    const meridiem = false;
+
+    const result = getHumanReadableTimeFormat({
+      value,
+      noSeconds,
+      utc,
+      meridiem
+    });
+
+    expect(result).toEqual(`Apr 23, 1993 0${hours}:33:12 UTC`);
+  });
+
+  it('returns full readable date without seconds in UTC, without meridiem', () => {
+    const date = new Date(1993, 3, 23, 8, 33, 12);
+    const value = date.getTime() / 1000; // UNIX timestamp
+    const noSeconds = true;
+    const utc = true;
+    const hours = date.getUTCHours();
+    const meridiem = false;
+
+    const result = getHumanReadableTimeFormat({
+      value,
+      noSeconds,
+      utc,
+      meridiem
+    });
+
+    expect(result).toEqual(`Apr 23, 1993 0${hours}:33 UTC`);
+  });
 });
