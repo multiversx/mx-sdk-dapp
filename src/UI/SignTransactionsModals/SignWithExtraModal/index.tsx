4bf59616923583837cbe7e4c4712af2137ad87c1
Stanislav Sava <sava.stanislav@gmail.com>
2023-02-03 12:51:26 +0200

    add SignTransactionModal for extra to default

---
+++ src/UI/SignTransactionsModals/SignWithExtraModal/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useSignTransactions } from 'hooks/transactions/useSignTransactions';
+
+export function SignWithExtraModal() {
+  useSignTransactions();
+  return <div />;
+}
