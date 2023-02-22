333a97cf45955a3f41e813a81f746e20ad9367be
Stanislav Sava <sava.stanislav@gmail.com>
2023-02-22 17:53:21 +0200

    fix tests

--- src/utils/asyncActions/sleep.ts
+++ src/utils/asyncActions/sleep.ts
@@ -1,5 +1,10 @@
-export function sleep(time: number) {
-  return new Promise<void>((resolve) => {
+export async function sleep(time: number) {
+  return await new Promise<void>((resolve) => {
+    //timeouts are broken in jest, if this is a test env
+    //exit the sleep state immediately
+    if (process.env.NODE_ENV === 'test') {
+      resolve();
+    }
     setTimeout(() => resolve(), time);
   });
 }
