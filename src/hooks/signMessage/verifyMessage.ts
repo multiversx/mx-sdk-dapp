1356fdca33a40d437fbc03cd3c50ad53c8fed550
Tudor Morar <tudor.morar@elrond.com>
2023-02-27 14:43:09 +0200

    Fix unable to verify

--- src/hooks/signMessage/verifyMessage.ts
+++ src/hooks/signMessage/verifyMessage.ts
@@ -3,21 +3,29 @@ import { Signature } from '@multiversx/sdk-core/out/signature';
 import { UserPublicKey, UserVerifier } from '@multiversx/sdk-wallet';
 
 export const verifyMessage = (signedMessage: string) => {
-  const { message, address, signature } = JSON.parse(signedMessage);
+  try {
+    const { message, address, signature } = JSON.parse(signedMessage);
 
-  const decodedMessage = Buffer.from(message.replace('0x', ''), 'hex');
-  const bech32Address = new Address(address);
-  const signedM = new SignableMessage({
-    address: bech32Address,
-    message: decodedMessage,
-    signature: new Signature(signature)
-  });
-  const publicKey = new UserPublicKey(Address.fromString(address).pubkey());
-  const verifier = new UserVerifier(publicKey);
+    const decodedMessage = Buffer.from(message.replace('0x', ''), 'hex');
+    const bech32Address = new Address(address);
+    const signedM = new SignableMessage({
+      address: bech32Address,
+      message: decodedMessage,
+      signature: new Signature(signature)
+    });
+    const publicKey = new UserPublicKey(Address.fromString(address).pubkey());
+    const verifier = new UserVerifier(publicKey);
 
-  return {
-    isVerified: verifier.verify(signedM),
-    message: decodedMessage.toString(),
-    address
-  };
+    return {
+      isVerified: verifier.verify(signedM),
+      message: decodedMessage.toString(),
+      address
+    };
+  } catch {
+    return {
+      isVerified: false,
+      message: '',
+      address: ''
+    };
+  }
 };
