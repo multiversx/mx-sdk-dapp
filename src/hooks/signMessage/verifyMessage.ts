b8a44ff8e3409fead63633761666411f78de62da
Tudor Morar <tudor.morar@elrond.com>
2023-02-27 14:08:41 +0200

    Turn useVerifyMessage into simple verifiyMessage helper

--- src/hooks/signMessage/verifyMessage.ts
+++ src/hooks/signMessage/verifyMessage.ts
@@ -2,15 +2,22 @@ import { Address, SignableMessage } from '@multiversx/sdk-core/out';
 import { Signature } from '@multiversx/sdk-core/out/signature';
 import { UserPublicKey, UserVerifier } from '@multiversx/sdk-wallet';
 
-export const useVerifyMessage = () => (signedMessage: string) => {
+export const verifyMessage = (signedMessage: string) => {
   const { message, address, signature } = JSON.parse(signedMessage);
+
+  const decodedMessage = Buffer.from(message.replace('0x', ''), 'hex');
+  const bech32Address = new Address(address);
   const signedM = new SignableMessage({
-    address: new Address(address),
-    message: Buffer.from(message.replace('0x', ''), 'hex'),
+    address: bech32Address,
+    message: decodedMessage,
     signature: new Signature(signature)
   });
   const publicKey = new UserPublicKey(Address.fromString(address).pubkey());
   const verifier = new UserVerifier(publicKey);
 
-  return verifier.verify(signedM);
+  return {
+    verified: verifier.verify(signedM),
+    message: decodedMessage,
+    address: bech32Address
+  };
 };
