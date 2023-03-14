fe3a942ce6ebb0811e5a478fb3b5f028715a5b11
Tudor Morar <tudor.morar@elrond.com>
2023-03-14 14:51:23 +0200

    Move getIsExtensionAvailable to utils

--- src/UI/extension/helpers/index.tsx
+++ src/UI/extension/helpers/index.tsx
@@ -1,1 +1,2 @@
-export * from './getIsExtensionAvailable';
+// TODO: v3 breaking change remove this file in v3 (breaking change) and use directly from utils
+export * from 'utils/platform/getIsExtensionAvailable';
