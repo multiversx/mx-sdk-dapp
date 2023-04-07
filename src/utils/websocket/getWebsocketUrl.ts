5e1f95a9d94864e2fbc2fa26fc59afeafa3d67c4
cipriandraghici <cipriandraghici93@gmail.com>
2023-04-08 01:43:40 +0300

    cleanup

--- src/utils/websocket/getWebsocketUrl.ts
+++ src/utils/websocket/getWebsocketUrl.ts
@@ -2,12 +2,10 @@ import axios from 'axios';
 
 export async function getWebsocketUrl(apiAddress: string) {
   try {
-    // @ts-ignore
     const { data } = await axios.get<{ url: string }>(
       `${apiAddress}/websocket/config`
     );
     return `wss://${data.url}`;
-    // return `wss://testnet-socket-extras-api.multiversx.com`;
   } catch (err) {
     console.error(err);
     throw new Error('Can not get websocket url');
