17859bc6a48c157948c62368fb9f91826c3ad6eb
Razvan Tomegea <razvan.tomegea@gmail.com>
2023-04-28 15:46:33 +0300

    [FIX] Fixed test names for `isAscii` helper

--- src/utils/decoders/tests/isAscii.test.ts
+++ src/utils/decoders/tests/isAscii.test.ts
@@ -1,34 +1,24 @@
 import { isAscii } from '../isAscii';
 
 describe('isAscii', () => {
-  it('should return true', async () => {
+  it('should return false for strings with unknown characters', async () => {
     const result = isAscii(
       '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~��������������������������������������������������������������������������������������������������������������������������������'
     );
     expect(result).toStrictEqual(false);
   });
 
-  it('should return true', async () => {
-    const result = isAscii('This ❌ h🅰s some 😱 emojis inside');
+  it('should return false for strings with emojis', async () => {
+    const result = isAscii('This ❌ h🅰s some 😱🙀 emojis inside');
     expect(result).toStrictEqual(false);
   });
 
-  it('should return true', async () => {
-    const result = isAscii('My Family 👨‍👩‍👧‍👦');
-    expect(result).toStrictEqual(false);
-  });
-
-  it('should return true', async () => {
-    const result = isAscii('test transaction 🙀');
-    expect(result).toStrictEqual(false);
-  });
-
-  it('should return false', async () => {
+  it('should return true for strings with symbols, letters, and numbers', async () => {
     const result = isAscii('ESDTTransfer@a129asnas98d@a9s8h98h9');
     expect(result).toStrictEqual(true);
   });
 
-  it('should return false', async () => {
+  it('should return true for simple strings with letters', async () => {
     const result = isAscii('Some example');
     expect(result).toStrictEqual(true);
   });
