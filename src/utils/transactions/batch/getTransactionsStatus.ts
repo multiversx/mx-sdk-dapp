4c23b536018f567c805fe3af97cec282d908430b
cipriandraghici <cipriandraghici93@gmail.com>
2023-05-02 19:07:31 +0300

    improve batch transactions tracker

---
+++ src/utils/transactions/batch/getTransactionsStatus.ts
@@ -0,0 +1,37 @@
+import { SignedTransactionType, TransactionServerStatusesEnum } from 'types';
+
+export const getTransactionsStatus = ({
+  transactions,
+  hasUnrelatedTransactions
+}: {
+  transactions: SignedTransactionType[];
+  hasUnrelatedTransactions?: boolean;
+}) => {
+  const allTxFailed = transactions.every(
+    ({ status }) => status === TransactionServerStatusesEnum.fail
+  );
+
+  const someTxFailed = transactions.some(
+    ({ status }) => status === TransactionServerStatusesEnum.fail
+  );
+
+  const isPending = transactions.some(
+    ({ status }) => status === TransactionServerStatusesEnum.pending
+  );
+
+  const isSuccessful = transactions.every(
+    ({ status }) => status === TransactionServerStatusesEnum.success
+  );
+
+  const isIncompleteFailed = hasUnrelatedTransactions
+    ? Boolean(!isPending && !allTxFailed && someTxFailed)
+    : false;
+
+  const isFailed = hasUnrelatedTransactions
+    ? isIncompleteFailed
+      ? false
+      : allTxFailed
+    : someTxFailed;
+
+  return { isPending, isSuccessful, isFailed, isIncompleteFailed };
+};
