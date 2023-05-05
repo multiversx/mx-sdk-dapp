cdcf4114d75a4e7dac5498f4e4b7337aae150472
cipriandraghici <cipriandraghici93@gmail.com>
2023-05-05 15:08:41 +0300

    fix constant value for TRANSACTIONS_STATUS_DROP_INTERVAL_MS

--- src/constants/transactionStatus.ts
+++ src/constants/transactionStatus.ts
@@ -2,4 +2,4 @@ export const TRANSACTION_STATUS_TOAST_ID = 'transaction-status-toast-id';
 export const AVERAGE_TX_DURATION_MS = 6000;
 export const CROSS_SHARD_ROUNDS = 5;
 export const TRANSACTIONS_STATUS_POLLING_INTERVAL_MS = 90 * 1000; // 90sec
-export const TRANSACTIONS_STATUS_DROP_INTERVAL_MS = 10 * 60 * 10000; // 10min
+export const TRANSACTIONS_STATUS_DROP_INTERVAL_MS = 10 * 60 * 1000; // 10min
