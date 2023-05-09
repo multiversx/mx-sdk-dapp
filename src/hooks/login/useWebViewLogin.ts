314daff856353cf1dfb506715cebfc8f70998c7d
Razvan Tomegea <razvan.tomegea@gmail.com>
2023-05-09 11:33:27 +0300

    [+] Added `getWebviewToken` helper

--- src/hooks/login/useWebViewLogin.ts
+++ src/hooks/login/useWebViewLogin.ts
@@ -1,20 +1,15 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'reduxStore/DappProviderContext';
 import { shouldUseWebViewProviderSelector } from 'reduxStore/selectors';
-
 import { loginWithNativeAuthToken } from 'services/nativeAuth/helpers/loginWithNativeAuthToken';
+import { getWebviewToken } from 'utils/account/getWebviewToken';
 
 export function useWebViewLogin() {
   const dispatch = useDispatch();
   const shouldUseWebViewProvider = useSelector(
     shouldUseWebViewProviderSelector
   );
-
-  const search = typeof window !== 'undefined' ? window?.location?.search : '';
-  const urlSearchParams = new URLSearchParams(search) as any;
-  const searchParams = Object.fromEntries(urlSearchParams);
-
-  const token = searchParams?.accessToken;
+  const token = getWebviewToken();
 
   useEffect(() => {
     if (!shouldUseWebViewProvider) {
