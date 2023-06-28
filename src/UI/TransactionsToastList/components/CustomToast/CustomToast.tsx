51407be2594d87b6a8b460e54be78d329e131483
cipriandraghici <cipriandraghici93@gmail.com>
2023-06-28 15:21:52 +0300

    poc custom toast component injection

--- src/UI/TransactionsToastList/components/CustomToast/CustomToast.tsx
+++ src/UI/TransactionsToastList/components/CustomToast/CustomToast.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-import { IconToast, SimpleToast } from './components';
-import { CustomToastPropsType } from './customToast.types';
+import { IconToast, SimpleToast, ComponentToast } from './components';
+import {
+  // ComponentIconToastPropsType,
+  CustomToastPropsType
+} from './customToast.types';
 import { useRemoveCustomToast } from './helpers';
 
 export const CustomToast = (props: CustomToastPropsType) => {
   const { duration, onDelete } = props;
   useRemoveCustomToast({ duration, onDelete });
+
+  if (props.component) {
+    const Component = props.component;
+    const newProps = {
+      ...props,
+      component: <Component />
+    } as any;
+
+    return <ComponentToast {...newProps} />;
+  }
 
   if (props.icon) {
     return <IconToast {...props} />;
---
+++ src/UI/TransactionsToastList/components/CustomToast/components/ComponentToast.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+import wrapperStyles from 'UI/TransactionsToastList/transactionsToastList.styles.scss';
+import { ComponentIconToastPropsType } from '../customToast.types';
+import { useMemoizedCloseButton } from '../helpers';
+
+export const ComponentToast = ({
+  onDelete,
+  message,
+  component,
+  CustomCloseButton,
+  className = 'dapp-custom-toast',
+  toastId
+}: ComponentIconToastPropsType) => {
+  const closeButton = useMemoizedCloseButton({ onDelete, CustomCloseButton });
+
+  console.log(component);
+
+  return (
+    <div
+      id={toastId}
+      className={classNames(
+        wrapperStyles.toasts,
+        wrapperStyles.toastWrapper,
+        className
+      )}
+    >
+      <>
+        {closeButton}
+        {component ? component : message}
+      </>
+    </div>
+  );
+};
