84f83aa6e5f47cc52a50ca81bae40d2382bc4195
Gavrila Andrei <andrei.gavrila@multiversx.com>
2023-06-30 23:18:48 +0300

    Add more tests for getWindowLocation

--- src/utils/window/tests/getWindowLocation.test.ts
+++ src/utils/window/tests/getWindowLocation.test.ts
@@ -1,5 +1,11 @@
 import { getWindowLocation } from '../getWindowLocation';
 
+const searchMock = '?search=mock';
+const pathnameMock = '/unlock';
+const originMock = 'https://multiversx.com';
+const hashMock = '#main';
+const hrefMock = 'https://multiversx.com/technology';
+
 let windowSpy: jest.SpyInstance;
 
 beforeEach(() => {
@@ -20,33 +26,55 @@ describe('Get window location', () => {
   it('window should return search', () => {
     windowSpy.mockImplementation(() => ({
       location: {
-        search: '?mock=test'
+        search: searchMock
       }
     }));
 
     const search = getWindowLocation('search');
-    expect(search).toStrictEqual('?mock=test');
+    expect(search).toStrictEqual(searchMock);
   });
 
   it('Window should return pathname', () => {
     windowSpy.mockImplementation(() => ({
       location: {
-        pathname: '/mock/test'
+        pathname: pathnameMock
       }
     }));
 
     const search = getWindowLocation('pathname');
-    expect(search).toStrictEqual('/mock/test');
+    expect(search).toStrictEqual(pathnameMock);
   });
 
   it('Window should return origin', () => {
     windowSpy.mockImplementation(() => ({
       location: {
-        origin: 'https://multiversx.com'
+        origin: originMock
       }
     }));
 
     const search = getWindowLocation('origin');
-    expect(search).toStrictEqual('https://multiversx.com');
+    expect(search).toStrictEqual(originMock);
+  });
+
+  it('Window should return hash', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        hash: hashMock
+      }
+    }));
+
+    const search = getWindowLocation('hash');
+    expect(search).toStrictEqual(hashMock);
+  });
+
+  it('Window should return href', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        href: hrefMock
+      }
+    }));
+
+    const search = getWindowLocation('href');
+    expect(search).toStrictEqual(hrefMock);
   });
 });
