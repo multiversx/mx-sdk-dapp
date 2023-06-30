7ef82ae44b0607a35332c1e3c47de80b4a017d67
Gavrila Andrei <andrei.gavrila@multiversx.com>
2023-06-30 17:37:04 +0300

    Fixed issue in which window is undefined in nextjs

--- src/utils/account/getSearchParamAddress.ts
+++ src/utils/account/getSearchParamAddress.ts
@@ -1,7 +1,8 @@
+import { isWindowAvailable } from 'utils';
 import { addressIsValid } from './addressIsValid';
 
 export const getSearchParamAddress = () => {
-  const search = window?.location.search;
+  const search = isWindowAvailable() ? window.location.search : '';
   const urlSearchParams = new URLSearchParams(search);
   const params = Object.fromEntries(urlSearchParams as any);
   const address: string = params?.address;
