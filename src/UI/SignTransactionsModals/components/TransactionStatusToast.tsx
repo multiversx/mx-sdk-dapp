b715044df7b534cd55c7951b7889803b392e1cc5
cipriandraghici <cipriandraghici93@gmail.com>
2023-07-05 10:07:35 +0300

    finalize cancel transaction toast, use the same toast container as the transactions toasts

--- src/UI/SignTransactionsModals/components/TransactionStatusToast.tsx
+++ src/UI/SignTransactionsModals/components/TransactionStatusToast.tsx
@@ -1,9 +1,10 @@
-import React, { useMemo } from 'react';
-
-import { FailedTransactionStatusToast } from 'components/TransactionStatusToast/FailedTransactionStatusToast';
+import React, { useEffect, useMemo } from 'react';
+import { faWarning } from '@fortawesome/free-solid-svg-icons';
+import { StatusMessageComponent } from 'components/TransactionStatusToast/StatusMessageComponent';
 import { StatusIconType } from 'components/TransactionStatusToast/transactionStatusToast.types';
 import { DEFAULT_TRANSACTION_STATUS_MESSAGE } from 'constants/index';
-
+import { useGetToasts } from 'hooks/toasts/useGetToasts';
+import { addNewCustomToast } from 'utils/toasts/customToastsActions';
 import { WithClassnameType } from '../../types';
 
 // TODO: Rename to "TransactionStatusToastPropsType" when sdk-dapp@3.0.0
@@ -17,9 +18,10 @@ export interface TransactionStatusToastType extends WithClassnameType {
 export const TransactionStatusToast = ({
   signError,
   canceledTransactionsMessage,
-  onDelete,
-  className
+  onDelete
 }: TransactionStatusToastType) => {
+  const { customToasts } = useGetToasts();
+
   const message = useMemo(() => {
     return (
       signError ||
@@ -38,12 +40,23 @@ export const TransactionStatusToast = ({
     return StatusIconType.INFO;
   }, [signError, canceledTransactionsMessage]);
 
-  return (
-    <FailedTransactionStatusToast
-      message={message}
-      type={type}
-      onDelete={onDelete}
-      className={className}
-    />
-  );
+  useEffect(() => {
+    if (
+      !customToasts.find((toast) => toast.toastId === 'failed-status-toast')
+    ) {
+      addNewCustomToast({
+        toastId: 'failed-status-toast',
+        title: 'Transaction canceled',
+        duration: 20000,
+        component: () => (
+          <StatusMessageComponent type={type} message={message} />
+        ),
+        icon: faWarning
+      });
+    }
+
+    return () => onDelete();
+  }, [customToasts, type, message, onDelete]);
+
+  return null;
 };
