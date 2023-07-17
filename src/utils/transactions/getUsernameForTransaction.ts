a9f5f9839b3643d44692df11b5cf1f8d0172ecd6
Tudor Morar <tudor.morar@multiversx.com>
2023-07-17 18:44:32 +0300

    Work on send usernames

---
+++ src/utils/transactions/getUsernameForTransaction.ts
@@ -0,0 +1,13 @@
+import { isStringBase64 } from '../decoders';
+
+export const getUsernameForTransaction = (username?: string) => {
+  if (!username) {
+    return undefined;
+  }
+
+  const defaultData = username ?? '';
+
+  return isStringBase64(defaultData)
+    ? defaultData
+    : Buffer.from(defaultData, 'base64').toString();
+};
