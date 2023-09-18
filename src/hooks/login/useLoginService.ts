a6491c1fb15c7f7f701c4fa327c5f1846c4abd1a
andreigiura <giura.andrei70@gmail.com>
2023-09-18 10:24:12 +0300

    Add network option when refreshing a native auth token (#927)
    * fix refresh token signature (#924)
    
    * 2.20.5
    
    * changelog
    
    * upgrade msw package to latest to fix tests
    
    * add  network option when refreshing a native auth token
    
    * changelog
    
    ---------
    
    Co-authored-by: cipriandraghici <cipriandraghici93@gmail.com>

--- src/hooks/login/useLoginService.ts
+++ src/hooks/login/useLoginService.ts
@@ -6,7 +6,7 @@ import { networkSelector, tokenLoginSelector } from 'reduxStore/selectors';
 import { setTokenLogin } from 'reduxStore/slices';
 import { nativeAuth } from 'services/nativeAuth';
 import { getNativeAuthConfig } from 'services/nativeAuth/methods';
-import { OnProviderLoginType } from 'types';
+import { NativeAuthConfigType, OnProviderLoginType } from 'types';
 
 const getApiAddress = (
   apiAddress: string,
@@ -99,14 +99,23 @@ export const useLoginService = (config?: OnProviderLoginType['nativeAuth']) => {
 
   // TODO: @StanislavSava verify and maybe refactor to separate function
   const refreshNativeAuthTokenLogin = async ({
-    signMessageCallback
+    signMessageCallback,
+    nativeAuthClientConfig
   }: {
     signMessageCallback: (
       messageToSign: SignableMessage,
       options: Record<any, any>
     ) => Promise<SignableMessage>;
+    nativeAuthClientConfig?: NativeAuthConfigType;
   }) => {
-    const loginToken = await getNativeAuthLoginToken();
+    const nativeAuthClient = nativeAuth(
+      nativeAuthClientConfig || configuration
+    );
+
+    const loginToken = await nativeAuthClient.initialize({
+      noCache: Boolean(nativeAuthClientConfig)
+    });
+
     tokenRef.current = loginToken;
     if (!loginToken) {
       return;
