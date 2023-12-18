8e77949195481e7faccccefb19ba621503dc35d3
cipriandraghici <cipriandraghici93@gmail.com>
2023-12-18 18:22:23 +0200

    changes on cross window provider types reexports

--- src/types/crossWindowProvider.types.ts
+++ src/types/crossWindowProvider.types.ts
@@ -1,1 +1,8 @@
-export * from '@multiversx/sdk-web-wallet-cross-window-provider/out/types';
+export {
+  CrossWindowProviderRequestEnums,
+  CrossWindowProviderResponseEnums,
+  ReplyWithPostMessageType,
+  ResponseTypeMap,
+  SignMessageStatusEnum,
+  ReplyWithPostMessageObjectType
+} from '@multiversx/sdk-web-wallet-cross-window-provider/out/types';
