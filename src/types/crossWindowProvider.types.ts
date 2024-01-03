b838f97918fd3d565e5afa183b36288301dd505e
Tudor Morar <tudor.morar@multiversx.com>
2024-01-03 17:40:28 +0200

    Upgrade @multiversx/sdk-web-wallet-cross-window-provider@0.0.10

--- src/types/crossWindowProvider.types.ts
+++ src/types/crossWindowProvider.types.ts
@@ -2,6 +2,7 @@ import {
   CrossWindowProviderRequestEnums,
   CrossWindowProviderResponseEnums,
   ReplyWithPostMessageType,
+  RequestMessageType,
   ResponseTypeMap,
   SignMessageStatusEnum,
   ReplyWithPostMessageObjectType
@@ -12,6 +13,7 @@ export {
   CrossWindowProviderResponseEnums,
   ReplyWithPostMessageType,
   ResponseTypeMap,
+  RequestMessageType,
   SignMessageStatusEnum,
   ReplyWithPostMessageObjectType
 };
