9c76729f99151ad620ded9546486f08a6f5e3c2a
Razvan Tomegea <razvan.tomegea@gmail.com>
2024-01-08 16:26:44 +0200

    Prevent logout before webview login

--- src/services/nativeAuth/helpers/loginWithNativeAuthToken.ts
+++ src/services/nativeAuth/helpers/loginWithNativeAuthToken.ts
@@ -1,4 +1,4 @@
-import { loginAction, logoutAction } from 'reduxStore/commonActions';
+import { loginAction } from 'reduxStore/commonActions';
 import { setTokenLogin } from 'reduxStore/slices';
 import { store } from 'reduxStore/store';
 import { LoginMethodsEnum } from 'types';
@@ -6,25 +6,23 @@ import { decodeNativeAuthToken } from './decodeNativeAuthToken';
 
 export function loginWithNativeAuthToken(token: string, dispatch?: any) {
   const nativeAuthInfo = decodeNativeAuthToken(token);
+
   if (nativeAuthInfo == null) {
     return;
   }
+
   const dispatchFn = dispatch ?? store.dispatch;
-
   const { signature, address } = nativeAuthInfo;
-  if (signature && token && address) {
-    //this will clear out the store from all previous logins
-    dispatchFn(logoutAction());
 
-    setTimeout(() => {
-      dispatchFn(
-        setTokenLogin({
-          loginToken: token,
-          signature,
-          nativeAuthToken: token
-        })
-      );
-      dispatchFn(loginAction({ address, loginMethod: LoginMethodsEnum.extra }));
-    });
+  if (signature && token && address) {
+    dispatchFn(
+      setTokenLogin({
+        loginToken: token,
+        signature,
+        nativeAuthToken: token
+      })
+    );
+
+    dispatchFn(loginAction({ address, loginMethod: LoginMethodsEnum.extra }));
   }
 }
