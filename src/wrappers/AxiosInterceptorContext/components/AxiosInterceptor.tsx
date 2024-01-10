9cb80863a691e918226c1740800316704d7c66a3
Tudor Morar <tudor.morar@multiversx.com>
2024-01-10 15:03:44 +0200

    Set interceptors

--- src/wrappers/AxiosInterceptorContext/components/AxiosInterceptor.tsx
+++ src/wrappers/AxiosInterceptorContext/components/AxiosInterceptor.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, PropsWithChildren, useEffect } from 'react';
 import axios from 'axios';
+import { axiosInstance } from 'apiCalls/axiosInstance';
 import { useAxiosInterceptorContext } from './AxiosInterceptorContextProvider';
 
 export interface AxiosInterceptorType extends PropsWithChildren {
@@ -13,40 +14,53 @@ export const AxiosInterceptor = ({
   const { loginInfo } = useAxiosInterceptorContext();
   const bearerToken = loginInfo?.tokenLogin?.nativeAuthToken;
 
-  const requestIdRef = useRef(-1);
+  const requestIdRef = useRef({
+    0: -1,
+    1: -1
+  });
 
   const setResponseInterceptors = () => {
-    axios.interceptors.response.use(
-      (response) => {
-        return response;
-      },
-      (error) => {
-        let url = error.config?.url;
-        if (error.config?.params) {
-          const queryString = new URLSearchParams(error.config.params);
-          url += `?${queryString.toString()}`;
+    const axiosResponse = axios.interceptors.response;
+    const axiosInstanceResponse = axios.interceptors.response;
+    [axiosResponse, axiosInstanceResponse].forEach((interceptor) => {
+      interceptor.use(
+        (response) => {
+          return response;
+        },
+        (error) => {
+          let url = error.config?.url;
+          if (error.config?.params) {
+            const queryString = new URLSearchParams(error.config.params);
+            url += `?${queryString.toString()}`;
+          }
+          console.error('Axios error for: ', url);
+          return Promise.reject(error);
         }
-        console.error('Axios error for: ', url);
-        return Promise.reject(error);
-      }
-    );
+      );
+    });
   };
 
   const setInterceptors = () => {
-    axios.interceptors.request.eject(requestIdRef.current);
+    axios.interceptors.request.eject(requestIdRef.current[0]);
+    axiosInstance.interceptors.request.eject(requestIdRef.current[1]);
 
-    requestIdRef.current = axios.interceptors.request.use(
-      async (config) => {
-        if (authenticatedDomains.includes(String(config?.baseURL))) {
-          config.headers.set('Authorization', `Bearer ${bearerToken}`);
+    const request = axios.interceptors.request;
+    const instanceRequest = axiosInstance.interceptors.request;
+
+    [request, instanceRequest].forEach((interceptor, index) => {
+      requestIdRef.current[index] = interceptor.use(
+        async (config) => {
+          if (authenticatedDomains.includes(String(config?.baseURL))) {
+            config.headers.set('Authorization', `Bearer ${bearerToken}`);
+          }
+
+          return config;
+        },
+        (error) => {
+          Promise.reject(error);
         }
-
-        return config;
-      },
-      (error) => {
-        Promise.reject(error);
-      }
-    );
+      );
+    });
   };
 
   useEffect(setResponseInterceptors, []);
