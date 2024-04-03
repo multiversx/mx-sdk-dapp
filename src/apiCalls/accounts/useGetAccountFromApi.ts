1307bcb1b0f92b4401c78dee7db97c0c63d599e2
Razvan Tomegea <razvan.tomegea@gmail.com>
2024-04-03 16:36:45 +0300

    Fix null address account fetch

--- src/apiCalls/accounts/useGetAccountFromApi.ts
+++ src/apiCalls/accounts/useGetAccountFromApi.ts
@@ -1,17 +1,9 @@
 import { ACCOUNTS_ENDPOINT } from '../endpoints';
 import { useApiFetch } from '../utils';
 
-export const useGetAccountFromApi = (address: string | null) => {
-  if (!address) {
-    return {
-      data: null,
-      isLoading: false,
-      error: false,
-      isValidating: false
-    };
-  }
-
-  return useApiFetch({
-    apiEndpoint: `${ACCOUNTS_ENDPOINT}/${address}?withGuardianInfo=true`
+export const useGetAccountFromApi = (address: string | null) =>
+  useApiFetch({
+    apiEndpoint: address
+      ? `${ACCOUNTS_ENDPOINT}/${address}?withGuardianInfo=true`
+      : null
   });
-};
--- src/apiCalls/utils/useApiFetch.ts
+++ src/apiCalls/utils/useApiFetch.ts
@@ -2,9 +2,13 @@ import useSWR from 'swr';
 import { axiosFetcher } from './axiosFetcher';
 import { getCleanApiAddress } from './getCleanApiAddress';
 
-export const useApiFetch = ({ apiEndpoint }: { apiEndpoint: string }) => {
+export const useApiFetch = ({
+  apiEndpoint
+}: {
+  apiEndpoint: string | null;
+}) => {
   const apiAddress = getCleanApiAddress();
-  const url = `${apiAddress}/${apiEndpoint}`;
+  const url = apiEndpoint ? `${apiAddress}/${apiEndpoint}` : null;
 
   return useSWR(url, axiosFetcher);
 };
