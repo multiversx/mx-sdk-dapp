0b1d2c125f3c36e4f4174a3c0a81c3858c87c239
Tudor Morar <tudor.morar@multiversx.com>
2024-04-17 12:55:20 +0300

    Revert changes to test file

--- src/utils/account/tests/getSearchParamAddress.test.ts
+++ src/utils/account/tests/getSearchParamAddress.test.ts
@@ -1,60 +1,43 @@
 import { cleanup } from '@testing-library/react';
 import { testAddress } from '__mocks__';
-import * as windowLocation from '../../../utils/window/getWindowLocation';
 import { getSearchParamAddress } from '../getSearchParamAddress';
 
-jest.mock('../../../utils/window/getWindowLocation', () => {
-  return {
-    __esModule: true,
-    ...jest.requireActual('../../../utils/window/getWindowLocation')
-  };
-});
+const createMockLocation = (search: string) => {
+  if (!global.window) {
+    global.window = Object.create(window);
+  }
 
-// const createMockLocation = (search: string) => {
-//   if (!global.window) {
-//     global.window = Object.create(window);
-//   }
-
-//   Object.defineProperty(window, 'location', {
-//     value: {
-//       search
-//     },
-//     writable: true
-//   });
-// };
+  Object.defineProperty(window, 'location', {
+    value: {
+      search
+    },
+    writable: true
+  });
+};
 
 describe('getSearchParamAddress', () => {
-  // afterEach(cleanup);
+  afterEach(cleanup);
   it('should return the address if it is valid', () => {
-    // createMockLocation(`?address=${testAddress}`);
+    createMockLocation(`?address=${testAddress}`);
 
-    jest.spyOn(windowLocation, 'getWindowLocation').mockReturnValue({
-      pathname: '',
-      hash: '',
-      origin: '',
-      href: '',
-      search: `?address=${testAddress}`
-    });
-    expect(1).toBe(1);
+    const result = getSearchParamAddress();
 
-    // const result = getSearchParamAddress();
-
-    // expect(result).toBe(testAddress);
+    expect(result).toBe(testAddress);
   });
 
-  // it('should return null if the address is invalid', () => {
-  //   createMockLocation('?address=invalidAddress');
+  it('should return null if the address is invalid', () => {
+    createMockLocation('?address=invalidAddress');
 
-  //   const result = getSearchParamAddress();
+    const result = getSearchParamAddress();
 
-  //   expect(result).toBeNull();
-  // });
+    expect(result).toBeNull();
+  });
 
-  // it('should return null if the address parameter is not provided', () => {
-  //   createMockLocation('');
+  it('should return null if the address parameter is not provided', () => {
+    createMockLocation('');
 
-  //   const result = getSearchParamAddress();
+    const result = getSearchParamAddress();
 
-  //   expect(result).toBeNull();
-  // });
+    expect(result).toBeNull();
+  });
 });
