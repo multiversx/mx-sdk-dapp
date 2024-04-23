7ca6841d0274edee2c02d75a8f2b39eb3368c987
Tudor Morar <tudor.morar@multiversx.com>
2024-04-23 17:10:05 +0300

    Update enums

--- src/lib/sdkDappUtils.ts
+++ src/lib/sdkDappUtils.ts
@@ -1,1 +1,5 @@
 export { safeWindow } from '@multiversx/sdk-dapp-utils/out/constants/crossWindowProviderConstants';
+export {
+  CrossWindowProviderRequestEnums,
+  CrossWindowProviderResponseEnums
+} from '@multiversx/sdk-dapp-utils/out/enums/crossWindowProviderEnums';
