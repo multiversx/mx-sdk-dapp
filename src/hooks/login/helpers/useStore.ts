b1cc2cdd3375bc1d5784a1cd659a89b24bde395e
Tudor Morar <tudor.morar@multiversx.com>
2024-04-27 12:10:34 +0300

    Build store working

--- src/hooks/login/helpers/useStore.ts
+++ src/hooks/login/helpers/useStore.ts
@@ -1,9 +1,5 @@
-import {
-  sessionNetworkStore,
-  RootState,
-  KeysEnum
-} from '@multiversx/sdk-dapp-core/dist/store/models/network';
-import { create } from 'zustand';
+import * as network from '@multiversx/sdk-dapp-core/dist/store/models/network';
+import { StoreApi, UseBoundStore, create } from 'zustand';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 function callSetFunction<T extends Function, F extends Function>(
@@ -16,23 +12,34 @@ function callSetFunction<T extends Function, F extends Function>(
   };
 }
 
-// Immer working
-export const useStore = create<RootState>((set) => {
-  const returnObj: any = {};
+function buildStore<T, K, S extends StoreApi<T>>({
+  KeysEnum,
+  store
+}: {
+  KeysEnum: K;
+  store: S;
+}) {
+  const useStore = create<T>((set) => {
+    const returnObj: any = {};
 
-  for (const key in KeysEnum) {
-    const networkKey = KeysEnum[key as keyof typeof KeysEnum];
-    const currentValue = sessionNetworkStore.getState()[networkKey];
+    for (const key in KeysEnum) {
+      const currentKey = KeysEnum[key as keyof typeof KeysEnum];
+      const currentValue = (store as any).getState()[currentKey];
 
-    returnObj[networkKey] =
-      typeof currentValue === 'function'
-        ? callSetFunction(currentValue, set)
-        : currentValue;
-  }
+      returnObj[currentKey] =
+        typeof currentValue === 'function'
+          ? callSetFunction(currentValue, set)
+          : currentValue;
+    }
 
-  return returnObj as RootState;
-});
+    return returnObj as T;
+  });
 
-sessionNetworkStore.subscribe((newState) => {
-  useStore.setState(newState);
-});
+  store.subscribe((newState) => {
+    useStore.setState(newState);
+  });
+
+  return useStore as UseBoundStore<StoreApi<ReturnType<typeof store.getState>>>;
+}
+
+export const useStore = buildStore(network);
