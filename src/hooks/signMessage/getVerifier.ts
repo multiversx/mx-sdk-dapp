6219a333859e9c212d6b5debe84d01774016c6a4
Tudor Morar <tudor.morar@multiversx.com>
2024-10-22 11:47:43 +0300

    Update sdk-core

--- src/hooks/signMessage/getVerifier.ts
+++ src/hooks/signMessage/getVerifier.ts
@@ -1,8 +1,7 @@
-import { Address } from '@multiversx/sdk-core/out';
-import { UserPublicKey, UserVerifier } from '@multiversx/sdk-wallet';
+import { Address, UserPublicKey, UserVerifier } from '@multiversx/sdk-core';
 
 export const getVerifier = (address: string) => {
-  const publicKey = new UserPublicKey(Address.fromString(address).pubkey());
+  const publicKey = new UserPublicKey(new Address(address).pubkey());
 
   return new UserVerifier(publicKey);
 };
