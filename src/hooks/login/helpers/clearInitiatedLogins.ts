3d72b717964785454c16e4973b7bfeb919b5e294
DanutIlie <42973343+DanutIlie@users.noreply.github.com>
2024-11-04 17:24:34 +0200

    update skip method to clear only initiated login state (#1298)
    * update skip method to clear only initiated login state
    
    * Update CHANGELOG.md
    
    * rename to intiatedLoginMethod

--- src/hooks/login/helpers/clearInitiatedLogins.ts
+++ src/hooks/login/helpers/clearInitiatedLogins.ts
@@ -2,9 +2,11 @@ import { CrossWindowProvider } from 'lib/sdkWebWalletCrossWindowProvider';
 import { IframeProvider } from 'lib/sdkWebWalletIframeProvider';
 import { LoginMethodsEnum } from 'types';
 
-export const clearInitiatedLogins = (props?: { skip: LoginMethodsEnum }) => {
+export const clearInitiatedLogins = (props?: {
+  intiatedLoginMethod: LoginMethodsEnum;
+}) => {
   Object.values(LoginMethodsEnum).forEach((method) => {
-    if (props?.skip && method === props.skip) {
+    if (props?.intiatedLoginMethod && method !== props.intiatedLoginMethod) {
       return;
     }
     const crossWindowProvider = CrossWindowProvider.getInstance();
