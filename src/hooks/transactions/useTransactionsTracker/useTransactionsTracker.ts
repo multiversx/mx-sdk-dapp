046aae0167242bfd447a2d7cfd45e5512adc4826
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-01-17 17:53:16 +0200

    Fix import order

--- src/hooks/transactions/useTransactionsTracker/useTransactionsTracker.ts
+++ src/hooks/transactions/useTransactionsTracker/useTransactionsTracker.ts
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 import { getTransactionsByHashes as defaultGetTxByHash } from 'apiCalls/transactions';
 import { TransactionsTrackerType } from 'types/transactionsTracker.types';
 import { useRegisterWebsocketListener } from '../../websocketListener';
+import { useCheckTransactionStatus } from '../useCheckTransactionStatus';
 import { useCheckHangingTransactionsFallback } from './useCheckHangingTransactionsFallback';
 import { useCheckTransactionOnWsFailureFallback } from './useCheckTransactionOnWsFailureFallback';
-import { useCheckTransactionStatus } from '../useCheckTransactionStatus';
 
 export function useTransactionsTracker(props?: TransactionsTrackerType) {
   const checkTransactionStatus = useCheckTransactionStatus();
