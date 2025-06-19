8493bbad020d14d32c71414d01f7447700e4bb2c
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2025-06-19 17:27:05 +0300

    Add utility for scam/suspicious content detection in asset messages, with supporting types and dependency. (#1458)
    * Add getScamFlag utility to identify suspicious content in messages
    
    This new utility checks for links, NSFW content, and scam information in messages, returning a flag indicating whether the content is suspicious. It includes a helper function to normalize links within the text.
    
    * Add SuspiciousLink types and export in index.ts
    
    This commit introduces a new file `suspiciousLink.types.ts` containing interfaces for handling suspicious links, including properties for scam information and NSFW content. The new types are also exported in `index.ts` for broader accessibility.
    
    * Update package.json and yarn.lock to add linkifyjs and upgrade sdk-web-wallet-iframe-provider to version 3.0.1
    
    * Update version to 5.0.0-alpha.8 and add release notes in CHANGELOG.md
    
    * Update CHANGELOG.md to correct pull request links for version 5.0.0-alpha.8

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ export * from './network.types';
 export * from './provider.types';
 export * from './subscriptions.type';
 export * from './websocket.types';
+export * from './suspiciousLink.types';
