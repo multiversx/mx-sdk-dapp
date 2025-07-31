c867f4b77ee7fcee798d50c62fc6fa66b4fc35f6
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-07-31 10:43:38 +0300

    Update getWindowParentOrigin

--- src/utils/window/getWindowParentOrigin.ts
+++ src/utils/window/getWindowParentOrigin.ts
@@ -1,24 +1,19 @@
 import { safeWindow } from 'constants/window.constants';
 
-export function getWindowParentOrigin() {
+export const getWindowParentOrigin = () => {
   if (!safeWindow) {
     return '';
   }
 
   try {
-    if (safeWindow.document.referrer) {
-      return new URL(safeWindow.document.referrer).origin;
-    }
+    const referrer = safeWindow.document?.referrer;
 
-    const ancestorOrigins = safeWindow.location.ancestorOrigins;
-
-    if (ancestorOrigins.length < 1) {
+    if (!referrer) {
       return '';
     }
 
-    return new URL(ancestorOrigins[ancestorOrigins.length - 1]).origin;
-  } catch (e) {
-    console.error(e);
+    return new URL(referrer).origin;
+  } catch {
     return '';
   }
-}
+};
