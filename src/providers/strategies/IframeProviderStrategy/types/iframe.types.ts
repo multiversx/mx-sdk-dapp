411402226bcf1ca004666607ae947c88ced1a28a
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-09-29 12:57:23 +0300

    Removed passkey and update metamask snap (#1597)
    * Remove passkey
    
    * Remove passkey
    
    * Update signTransactions
    
    * Update iframe to stable version
    
    * Update changelog

--- src/providers/strategies/IframeProviderStrategy/types/iframe.types.ts
+++ src/providers/strategies/IframeProviderStrategy/types/iframe.types.ts
@@ -1,7 +1,4 @@
-import { IframeLoginTypes } from '@multiversx/sdk-web-wallet-iframe-provider/out/constants';
-
 export type IframeProviderType = {
-  type: IframeLoginTypes;
   address?: string;
   walletUrl?: string;
 };
