dd42e383b54eacf6e12d64f0b2e0943b40e4141e
Tudor Morar <tudor.morar@multiversx.com>
2025-09-30 18:02:48 +0300

    Add log

--- src/managers/TransactionManager/TransactionManager.ts
+++ src/managers/TransactionManager/TransactionManager.ts
@@ -115,6 +115,8 @@ export class TransactionManager {
     sentTransactions: SignedTransactionType[] | SignedTransactionType[][],
     options: TransactionManagerTrackOptionsType = { disableToasts: false }
   ): Promise<string> => {
+    console.log('props', { sentTransactions, options });
+
     const flatTransactions = this.sequentialToFlatArray(sentTransactions).map(
       (transaction) => ({
         ...transaction,
