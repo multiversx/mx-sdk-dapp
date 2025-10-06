4bbb9ac416bec285cd8bd365a7fc4f800a691c09
Razvan Tomegea <razvan.tomegea@gmail.com>
2025-10-06 13:18:07 +0300

    Refactor sender validation logic in `checkIsValidSender` to allow bypassing for specific origins
    * Removed the origin whitelist parameter and simplified the logic to allow sender validation bypass only for the 'wallet.multiversx.com' origin.
    * This change enhances security by restricting the bypass functionality to a specific trusted origin, improving the overall integrity of sender validation.

--- src/hooks/transactions/helpers/checkIsValidSender.ts
+++ src/hooks/transactions/helpers/checkIsValidSender.ts
@@ -1,27 +1,19 @@
+import { safeWindow } from 'lib/sdkDappUtils';
 import { AccountType } from 'types/account.types';
-
-const originWhiteLists = ['multiversx.com'];
 
 // Don't allow signing if the logged in account's address
 // is neither the sender or the sender account's active guardian
 export const checkIsValidSender = (
   senderAccount: Partial<AccountType> | null,
-  address: string | string[],
-  originWhiteList?: string
+  address: string | string[]
 ) => {
   if (!senderAccount) {
     return true;
   }
 
-  // Allow sender to be different than the logged in account's address if the origin is in the white list
-  if (originWhiteList) {
-    const isOriginWhiteList = originWhiteLists.some((origin) =>
-      originWhiteList.endsWith(origin)
-    );
-
-    if (isOriginWhiteList) {
-      return true;
-    }
+  // Allow sender to be different than the logged in address if the origin is multiversx.com
+  if (safeWindow?.location?.origin?.endsWith('wallet.multiversx.com')) {
+    return true;
   }
 
   if (Array.isArray(address)) {
