a41599746dcaed8023beb03e56833301ee4d4367
Tudor Morar <tudor.morar@multiversx.com>
2025-10-17 11:25:56 +0300

    Remove unused prop

--- src/managers/ToastManager/helpers/createToastsFromTransactions.ts
+++ src/managers/ToastManager/helpers/createToastsFromTransactions.ts
@@ -18,19 +18,15 @@ interface CreateToastsFromTransactionsReturnType {
 }
 
 interface CreateToastsFromTransactionsParamsType {
-  existingCompletedTransactions?: ITransactionToast[];
   store?: StoreType;
 }
 
-export async function createToastsFromTransactions({
-  existingCompletedTransactions = [],
-  ...props
-}: CreateToastsFromTransactionsParamsType): Promise<CreateToastsFromTransactionsReturnType> {
-  const store = props.store ?? getState();
+export async function createToastsFromTransactions(
+  props?: CreateToastsFromTransactionsParamsType
+): Promise<CreateToastsFromTransactionsReturnType> {
+  const store = props?.store ?? getState();
   const pendingTransactionToasts: ITransactionToast[] = [];
-  const completedTransactionToasts: ITransactionToast[] = [
-    ...existingCompletedTransactions
-  ];
+  const completedTransactionToasts: ITransactionToast[] = [];
   const toastList = toastsSliceSelector(store);
   const transactionsSessions = transactionsSliceSelector(store);
   const address = addressSelector(store);
