bb064c5cce5b517d1363641178b9a8d9587a7aa5
Tudor Morar <tudor.morar@multiversx.com>
2025-10-22 14:00:24 +0300

    Added tests

---
+++ src/managers/UnlockPanelManager/helpers/tests/getIsSimpleLoginCallback.test.ts
@@ -0,0 +1,24 @@
+import { getIsSimpleLoginCallback } from '../getIsSimpleLoginCallback';
+import { LoginHandlerType } from '../../UnlockPanelManager.types';
+
+describe('getIsSimpleLoginCallback tests', () => {
+  it('should return true for a function with zero parameters', () => {
+    const simpleCallback: LoginHandlerType = () => {
+      console.log('Simple callback');
+    };
+
+    const result = getIsSimpleLoginCallback(simpleCallback);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false for a function with one parameter', () => {
+    const loginHandler: LoginHandlerType = async ({ type, anchor }) => {
+      console.log('Login handler with parameters', type, anchor);
+    };
+
+    const result = getIsSimpleLoginCallback(loginHandler);
+
+    expect(result).toBe(false);
+  });
+});
