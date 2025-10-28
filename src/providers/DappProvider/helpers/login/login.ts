8969ea89c5db6aa6f18683de877128a5ed6467fe
Tudor Morar <tudor.morar@multiversx.com>
2025-10-28 11:16:15 +0200

    Extract login methods

--- src/providers/DappProvider/helpers/login/login.ts
+++ src/providers/DappProvider/helpers/login/login.ts
@@ -1,120 +1,18 @@
 import { registerWebsocketListener } from 'methods/initApp/websocket/registerWebsocket';
-import { getNetworkConfig } from 'methods/network/getNetworkConfig';
 import { trackTransactions } from 'methods/trackTransactions/trackTransactions';
 import { IProvider } from 'providers/types/providerFactory.types';
-import { nativeAuth } from 'services/nativeAuth';
-import { decodeNativeAuthToken } from 'services/nativeAuth/helpers/decodeNativeAuthToken';
-import { NativeAuthConfigType } from 'services/nativeAuth/nativeAuth.types';
-import { setTokenLogin } from 'store/actions/loginInfo/loginInfoActions';
 import { nativeAuthConfigSelector } from 'store/selectors';
 import { getState } from 'store/store';
-import { accountLogin } from './helpers/accountLogin';
-import { extractAddressFromToken } from './helpers/extractAddressFromToken';
-import { getMultisigLoginToken } from './helpers/getMultisigLoginToken';
+import { loginWithNativeToken } from './helpers/loginWithNativeToken';
+import { loginWithoutNativeToken } from './helpers/loginWithoutNativeToken';
 
-async function loginWithoutNativeToken(provider: IProvider) {
-  const {
-    network: { apiAddress }
-  } = getNetworkConfig();
-
-  await provider.login();
-
-  const address = await provider.getAddress();
-
-  if (!address) {
-    throw new Error('Address not found');
-  }
-
-  await accountLogin({ address, provider, apiAddress });
-
-  return {
-    address
-  };
-}
-
-async function loginWithNativeToken({
-  provider,
-  nativeAuthConfig,
-  token
-}: {
-  provider: IProvider;
-  nativeAuthConfig: NativeAuthConfigType;
-  token?: string;
-}) {
-  const {
-    network: { apiAddress }
-  } = getNetworkConfig();
-
-  const nativeAuthClient = nativeAuth(nativeAuthConfig);
-  let loginToken = token;
-
-  if (!loginToken) {
-    loginToken = await nativeAuthClient.initialize({
-      noCache: true
-    });
-  }
-
-  const loginData = await provider.login({
-    token: loginToken
-  });
-
-  const { address, signature, ...loginResult } = loginData;
-
-  if (!address) {
-    console.warn('Login cancelled.');
-    return null;
-  }
-
-  if (!signature) {
-    console.error('Failed to sign login token');
-    return null;
-  }
-
-  // nativeAuthToken received from hub login
-  const decodedToken = decodeNativeAuthToken(loginResult?.accessToken);
-
-  const usedLoginToken = await getMultisigLoginToken({
-    loginData,
-    nativeAuthConfig,
-    loginToken
-  });
-
-  const nativeAuthToken = decodedToken
-    ? loginResult.accessToken
-    : nativeAuthClient.getToken({
-        address,
-        token: usedLoginToken,
-        signature
-      });
-
-  setTokenLogin({
-    loginToken,
-    signature,
-    nativeAuthToken
-  });
-
-  const extractedAddress = await extractAddressFromToken({
-    loginToken,
-    extraInfoData: {
-      multisig: loginResult?.multisig,
-      impersonate: loginResult?.impersonate
-    },
-    address
-  });
-
-  await accountLogin({
-    address: extractedAddress,
-    provider,
-    apiAddress
-  });
-
-  return {
-    address: extractedAddress,
-    signature
-  };
-}
-
-export async function login(provider: IProvider, options?: { token?: string }) {
+export async function login(
+  provider: IProvider,
+  options?: { token?: string }
+): Promise<{
+  address: string;
+  signature: string;
+} | null> {
   const nativeAuthConfig = nativeAuthConfigSelector(getState());
 
   if (nativeAuthConfig) {
