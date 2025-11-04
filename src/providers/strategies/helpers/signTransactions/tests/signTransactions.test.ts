13e2d03b99ac97cf5c91088e12925460f763a532
Tudor Morar <tudor.morar@multiversx.com>
2025-11-04 15:28:13 +0200

    Work on second test

--- src/providers/strategies/helpers/signTransactions/tests/signTransactions.test.ts
+++ src/providers/strategies/helpers/signTransactions/tests/signTransactions.test.ts
@@ -36,6 +36,7 @@ jest.mock(
       notifyDataUpdate: jest.fn(() => {}),
       initializeGasPriceMap: jest.fn(() => {}),
       updateIsLoading: jest.fn(() => {}),
+      updateGasPriceMap: jest.fn(() => {}),
       getGasPriceOptionMap: {
         [1345]: {
           gasPriceOption: 1000000000,
@@ -71,6 +72,44 @@ describe('signTransactions tests', () => {
     await new Promise((resolve) => setTimeout(resolve));
 
     expect(subscribed).toHaveBeenCalled();
+    const [_confirmAction, confirmHandler] = subscribed.mock.calls.find(
+      ([call]) => call.includes('CONFIRM_SIGN_TRANSACTIONS')
+    );
+
+    await confirmHandler();
+
+    const signedTransactions = await signPromise;
+
+    expect(signedTransactions.map((el) => el.toPlainObject())).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(mockPlainTransactionObject)
+      ])
+    );
+  });
+  it('should sign a single transaction with a faster gas price', async () => {
+    const signPromise = signTransactions({
+      ...mockSignTransactionsInputData,
+      handleSign: mockHandleSign
+    });
+    const {
+      SignTransactionsStateManager
+    } = require('managers/internal/SignTransactionsStateManager/SignTransactionsStateManager');
+
+    const subscribed = SignTransactionsStateManager.getInstance()
+      .subscribeToEventBus as jest.Mock;
+
+    // allow async setup to register subscriptions
+    await new Promise((resolve) => setTimeout(resolve));
+
+    expect(subscribed).toHaveBeenCalled();
+
+    const [_setGasPriceOptionAction, setGasPriceOptionHandler] =
+      subscribed.mock.calls.find(([call]) =>
+        call.includes('SET_GAS_PRICE_OPTION_SIGN_TRANSACTIONS')
+      );
+
+    await setGasPriceOptionHandler(1050000000);
+
     const [_confirmAction, confirmHandler] = subscribed.mock.calls.find(
       ([call]) => call.includes('CONFIRM_SIGN_TRANSACTIONS')
     );
