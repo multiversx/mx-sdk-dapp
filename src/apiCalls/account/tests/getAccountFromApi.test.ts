3a193b27afcfcc646690bfca79fc2d462be6bc66
Tudor Morar <tudor.morar@multiversx.com>
2025-11-07 12:05:07 +0200

    Added getAccountFromApi tests

---
+++ src/apiCalls/account/tests/getAccountFromApi.test.ts
@@ -0,0 +1,45 @@
+import { server, rest, testNetwork } from '__mocks__';
+import { ACCOUNTS_ENDPOINT } from 'apiCalls/endpoints';
+import { getAccountFromApi } from '../getAccountFromApi';
+
+describe('getAccountFromApi tests', () => {
+  it('should return the account data', async () => {
+    const account = await getAccountFromApi({
+      address: '',
+      baseURL: testNetwork.apiAddress
+    });
+    expect(account).toBeNull();
+  });
+  it('should return null and log an error if account is not found', async () => {
+    const missingAccountAddress =
+      'erd1qqqqqqqqqqqqqpgqc978caxxxxxxxxxxxxxxxxxxxxxxxxxxxxplllss';
+
+    const consoleSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementation(() => undefined);
+
+    server.use(
+      rest.get(
+        `${testNetwork.apiAddress}/${ACCOUNTS_ENDPOINT}/${missingAccountAddress}`,
+        (_req, res, ctx) => {
+          return res(ctx.status(404), ctx.json({ error: 'account not found' }));
+        }
+      )
+    );
+
+    try {
+      const account = await getAccountFromApi({
+        address: missingAccountAddress,
+        baseURL: testNetwork.apiAddress
+      });
+
+      expect(account).toBeNull();
+      expect(consoleSpy).toHaveBeenCalledWith(
+        'error fetching configuration for ',
+        missingAccountAddress
+      );
+    } finally {
+      consoleSpy.mockRestore();
+    }
+  });
+});
