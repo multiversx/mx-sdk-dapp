ec9b1d0ec72cca6cca852b4060f34dbc62a56411
Tudor Morar <tudor.morar@multiversx.com>
2025-11-10 08:45:50 +0200

    Added switchTheme tests

---
+++ src/utils/visual/tests/switchTheme.test.ts
@@ -0,0 +1,28 @@
+import { safeWindow } from 'constants/window.constants';
+import { ThemesEnum } from 'types';
+import { switchTheme } from '../switchTheme';
+
+jest.mock('constants/window.constants', () => {
+  const dataset: Record<string, string> = {};
+  const mockWindow = {
+    document: {
+      documentElement: {
+        dataset
+      }
+    }
+  };
+  return {
+    get safeWindow() {
+      return mockWindow;
+    }
+  };
+});
+
+describe('switchTheme tests', () => {
+  it('should set the theme on document element dataset', () => {
+    switchTheme(ThemesEnum.dark);
+    expect(safeWindow.document.documentElement.dataset.mvxTheme).toBe(
+      ThemesEnum.dark
+    );
+  });
+});
