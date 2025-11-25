12c3d114fd7a11f5dae08b297575e21c4374b11d
Gavrila Andrei <andrei.gavrila@multiversx.com>
2025-11-25 16:59:50 +0200

    Test comm

--- src/providers/DappProvider/DappProvider.ts
+++ src/providers/DappProvider/DappProvider.ts
@@ -33,6 +33,7 @@ export class DappProvider {
   }
 
   isInitialized(): boolean {
+    // Directly return the result of the provider's isInitialized method programaticaly
     return this.provider.isInitialized();
   }
 
